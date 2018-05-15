eecefe36aec9c4372892acb4e4aeba659456051b
John Lewis <johnflewis93@gmail.com>
2018-05-15 22:31:36 +0100

    finish Maint script

--- maintenance/populateWikiSettings.php
+++ maintenance/populateWikiSettings.php
@@ -1,30 +1,45 @@
 <?php
 
-require_once '/srv/mediawiki/w/maintenance/commandLine.inc';
+require_once "$IP/maintenance/Maintenance.php";
 
-$dbw = wfGetDB( DB_MASTER );
+class ManageWikiPopulateSettings extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( 'wgsetting', 'The $wg setting minus $.', true );
+		$this->addOption( 'sourcelist', 'File in format of "wiki|value" for the $wg setting above.', true );
+	}
 
-$settingsource = file( "" );
+	function execute() {
+		$dbw = wfGetDB( DB_MASTER );
 
-foreach ( $settingsource as $input ) {
-	$wikiDB = explode( '|' $line, 2 );
-	list( $DBname, $settingvalue ) = array_pad( $wikiDB, 2, '' );
+		$settingsource = file( $this->getOption( 'sourcelist' ) );
 
-	$remoteWiki = RemoteWiki::newFromName
+		foreach ( $settingsource as $input ) {
+			$wikiDB = explode( '|' $line, 2 );
+			list( $DBname, $settingvalue ) = array_pad( $wikiDB, 2, '' );
 
-	$settingsarray = 
+			$remoteWiki = RemoteWiki::newFromName( $DBname );
 
-	$settingsarray[] = str_replace( "\n", '', $settingvalue );
+			$settingsarray = $remotewiki->getSettings();
 
-	$settings = json_encode( $settingsarray );
+			$settingsarray[$this->getOption('wgsetting')] = str_replace( "\n", '', $settingvalue );
 
-	$dbw->update( 'cw_wikis',
-		array(
-			'wiki_settings' => $settings
-		),
-		array(
-			'wiki_dbname' => $DBname
-		),
-		__METHOD__
-	);
+			$settings = json_encode( $settingsarray );
+
+			$dbw->update( 'cw_wikis',
+				array(
+					'wiki_settings' => $settings
+				),
+				array(
+					'wiki_dbname' => $DBname
+				),
+				__METHOD__
+			);
+
+			unset( $
+		}
+	}
 }
+
+$maintClass = 'ManageWikiPopulateSettings';
+require_once RUN_MAINTENANCE_IF_MAIN;
