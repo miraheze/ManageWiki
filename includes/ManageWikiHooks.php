7b5de543ff0625bb3919b0fba59dd6294451794b
paladox <paladox@users.noreply.github.com>
2018-07-25 18:26:11 +0100

    Update ManageWikiHooks.php

--- includes/ManageWikiHooks.php
+++ includes/ManageWikiHooks.php
@@ -1,4 +1,5 @@
 <?php
+
 class ManageWikiHooks {
         public static function onRegistration() {
                 global $wgLogTypes;
@@ -6,23 +7,5 @@ class ManageWikiHooks {
                 if ( !in_array( 'farmer', $wgLogTypes ) ) {
                         $wgLogTypes[] = 'farmer';
                 }
-        }
-        
-        public static function getTimezoneList() {
-                $identifiers = DateTimeZone::listIdentifiers(DateTimeZone::ALL);
-
-                $timeZoneList = [];
-                
-                if ( $identifiers !== false ) {
-                        foreach ( $identifiers as $identifier ) {
-                                $parts = explode( '/', $identifier, 2 );
-                                if ( count( $parts ) !== 2 && $parts[2] === 'Etc/Utc' ) {
-                                        continue;
-                                }
-                                $timeZoneList[$identifier] = $identifier;
-                        }
-                }
-                
-                return $timeZoneList;
         }
 }
