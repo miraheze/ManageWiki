a2be5e99a104cb55c0e8379fb77face930fd792a
paladox <paladox@users.noreply.github.com>
2018-07-25 18:16:31 +0100

    Add getTimezoneList static functions to hooks

--- includes/ManageWikiHooks.php
+++ includes/ManageWikiHooks.php
@@ -7,4 +7,22 @@ class ManageWikiHooks {
                         $wgLogTypes[] = 'farmer';
                 }
         }
+        
+        public static function getTimezoneList() {
+                $identifiers = DateTimeZone::listIdentifiers(DateTimeZone::ALL);
+
+                $timeZoneList = [];
+                
+                if ( $identifiers !== false ) {
+                        foreach ( $identifiers as $identifier ) {
+                                $parts = explode( '/', $identifier, 2 );
+                                if ( count( $parts ) !== 2 && $parts[2] === 'Etc/Utc' ) {
+                                        continue;
+                                }
+                                $timeZoneList[$identifier] = $identifier;
+                        }
+                }
+                
+                return $timeZoneList;
+        }
 }
