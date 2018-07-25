c2d3ade7a8a9a8fc7b5d04c9bbc5607cd247ab64
paladox <paladox@users.noreply.github.com>
2018-07-25 18:43:25 +0100

    Update ManageWiki.php

--- includes/ManageWiki.php
+++ includes/ManageWiki.php
@@ -9,7 +9,7 @@ class ManageWiki {
                 if ( $identifiers !== false ) {
                         foreach ( $identifiers as $identifier ) {
                                 $parts = explode( '/', $identifier, 2 );
-                                if ( count( $parts ) !== 2 && $parts[0] === 'UTC' ) {
+                                if ( count( $parts ) !== 2 && $parts[0] !== 'UTC' ) {
                                         continue;
                                 }
                                 $timeZoneList[$identifier] = $identifier;
