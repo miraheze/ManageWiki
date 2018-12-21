b9d7e88984669cf8c9ef8d9e1fd9e206c6ea3f02
John Lewis <johnflewis93@gmail.com>
2018-12-21 01:38:21 +0000

    add populateNamespaces.php

---
+++ maintenance/populateNamespaces.php
@@ -0,0 +1,49 @@
+<?php
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+class ManageWikiPopulateNamespaces extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+	}
+
+	function execute() {
+		global $wgCreateWikiDatabase, $wgDBname, $wgCanonicalNamespaceNames, $wgNamespaceAliases, $wgNamespacesToBeSearchedDefault, $wgNamespacesWithSubpages, $wgContentNamespaces, $wgNamespaceProtection;
+
+		$dbw = wfGetDB( DB_MASTER, [], $wgCreateWikiDatabase );
+
+		$namespaces = $wgCanonicalNamespaceNames + [ 0 => '<Main>' ];
+
+		foreach ( $namespaces as $id => $name ) {
+			$matchedNSKeys = array_keys( $wgNamespaceAliases, $id );
+			$nsAliases = [];
+
+			foreach ( $matchedNSKeys as $o => $n ) {
+				$nsAliases[] = $n;
+			}
+
+			$dbw->insert(
+				'mw_namespaces',
+				[
+					'ns_dbname' => $wgDBname,
+					'ns_namespace_id' => (int)$id,
+					'ns_namespace_name' => (string)$name,
+					'ns_searchable' => (int)$wgNamespacesToBeSearchedDefault[$id],
+					'ns_subpages' => (int)$wgNamespacesWithSubpages[$id],
+					'ns_content' => (int)$wgContentNamespaces,
+					'ns_protection' => ( is_array( $wgNamespaceProtection[$id] ) ) ? (string)$wgNamespaceProtection[$id][0] : (string)$wgNamespaceProtection[$id],
+					'ns_aliases' => (string)json_encode( $nsAliases ),
+					'ns_core' => ( $id < 1000 ) ? 1 : 0 // we assume less than < is "core", could do with smarter logic!
+				],
+				__METHOD__
+			);
+		}
+	}
+}
+
+$maintClass = 'ManageWikiPopulateNamespaces';
+require_once RUN_MAINTENANCE_IF_MAIN;
