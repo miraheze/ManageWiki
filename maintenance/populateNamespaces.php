d8617227974d445a0f0f0248adb64f2e49227880
paladox <paladox@users.noreply.github.com>
2019-01-05 23:46:57 +0000

    Fix populateNamespaces script to not insert duplicate data if ns id m… (#53)
    Fix populateNamespaces script to not insert duplicate data if ns id matches

--- maintenance/populateNamespaces.php
+++ maintenance/populateNamespaces.php
@@ -12,7 +12,7 @@ class ManageWikiPopulateNamespaces extends Maintenance {
 	}
 
 	function execute() {
-		global $wgCreateWikiDatabase, $wgDBname, $wgCanonicalNamespaceNames, $wgNamespaceAliases, $wgNamespacesToBeSearchedDefault, $wgNamespacesWithSubpages, $wgContentNamespaces, $wgNamespaceProtection;
+		global $wgCreateWikiDatabase, $wgDBname, $wgCanonicalNamespaceNames, $wgNamespaceAliases;
 
 		if ( ManageWiki::checkSetup( 'namespaces' ) ) {
 			$this->fatalError( 'Disable ManageWiki Namespaces on this wiki.' );
@@ -37,24 +37,52 @@ class ManageWikiPopulateNamespaces extends Maintenance {
 				$nsAliases[] = $n;
 			}
 
-			$dbw->insert(
+			$res = $dbw->select(
 				'mw_namespaces',
 				[
-					'ns_dbname' => $wgDBname,
-					'ns_namespace_id' => (int)$id,
-					'ns_namespace_name' => (string)$name,
-					'ns_searchable' => (int)$wgNamespacesToBeSearchedDefault[$id],
-					'ns_subpages' => (int)$wgNamespacesWithSubpages[$id],
-					'ns_content' => (int)$wgContentNamespaces[$id],
-					'ns_protection' => ( is_array( $wgNamespaceProtection[$id] ) ) ? (string)$wgNamespaceProtection[$id][0] : (string)$wgNamespaceProtection[$id],
-					'ns_aliases' => (string)json_encode( $nsAliases ),
-					'ns_core' => ( $id < 1000 ) ? 1 : 0 // we assume less than < is "core", could do with smarter logic!
+					'ns_namespace_name',
+					'ns_namespace_id'
+				],
+				[
+					'ns_dbname' => $wgDBname
 				],
 				__METHOD__
 			);
+
+
+			if ( !$res || !is_object( $res ) ) {
+				$this->insertNamespace( $dbw, $id, $name, $nsAliases);
+			} else {
+				foreach ( $res as $row ) {
+					if ( $row->ns_namespace_id !== (int)$id ) {
+						$this->insertNamespace( $dbw, $id, $name, $nsAliases);
+					}
+				}
+			}
 		}
 
 		Wikimedia\restoreWarnings();
+	}
+	
+	public function insertNamespace( $dbw, $id, $name, $nsAliases ) {
+		global $wgDBname, $wgNamespacesToBeSearchedDefault, $wgNamespacesWithSubpages,
+			$wgContentNamespaces, $wgNamespaceProtection;
+
+		$dbw->insert(
+			'mw_namespaces',
+			[
+				'ns_dbname' => $wgDBname,
+				'ns_namespace_id' => (int)$id,
+				'ns_namespace_name' => (string)$name,
+				'ns_searchable' => (int)$wgNamespacesToBeSearchedDefault[$id],
+				'ns_subpages' => (int)$wgNamespacesWithSubpages[$id],
+				'ns_content' => (int)$wgContentNamespaces[$id],
+				'ns_protection' => ( is_array( $wgNamespaceProtection[$id] ) ) ? (string)$wgNamespaceProtection[$id][0] : (string)$wgNamespaceProtection[$id],
+				'ns_aliases' => (string)json_encode( $nsAliases ),
+				'ns_core' => ( $id < 1000 ) ? 1 : 0 // we assume less than < is "core", could do with smarter logic!
+			],
+			__METHOD__
+		);
 	}
 }
 
