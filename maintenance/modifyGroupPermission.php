07d128ff956803bb4831a3807254ca1e8954a217
MacFan4000 <paulfkeffer@gmail.com>
2019-01-11 17:27:34 -0500

    Expand script

--- maintenance/modifyGroupPermission.php
+++ maintenance/modifyGroupPermission.php
@@ -12,13 +12,21 @@ class ManageWikiModifyGroupPermission extends Maintenance {
 		$this->addArg( 'group', 'The group name you want to change.', true );
 		$this->addOption( 'addperms', 'Comma separated list of permissions to add.', false, true );
 		$this->addOption( 'removeperms', 'Comma separated list of permissions to remove.', false, true );
+		$this->addOption( 'newaddgroups', 'Comma separated list of groups to add to the list of addable groups.', false, true );
+		$this->addOption( 'removeaddgroups', 'Comma separated list of groups to remove from the list of addable groups.', false, true );
+		$this->addOption( 'newremovegroups', 'Comma separated list of groups to add to the list of removable groups.', false, true );
+		$this->addOption( 'removeremovegroups', 'Comma separated list of groups to remove from the list of removable groups.', false, true );
 	}
 
 	function execute() {
 		$addp = (array)explode( ',', $this->getOption( 'addperms', '' ) );
 		$removep = (array)explode( ',', $this->getOption( 'removeperms', '' ) );
+		$addag = (array)explode( ',', $this->getOption( 'newaddgroups', '' ) );
+		$removeag = (array)explode( ',', $this->getOption( 'removeaddgroups', '' ) );
+		$addrg = (array)explode( ',', $this->getOption( 'newremovegroups', '' ) );
+		$removerg = (array)explode( ',', $this->getOption( 'removeremovegroups', '' ) );
 
-		ManageWiki::modifyPermissions( $this->getArg( 0 ), $addp, $removep );
+		ManageWiki::modifyPermissions( $this->getArg( 0 ), $addp, $removep, $addag, $removeag, $addrg, $removerg );
 	}
 }
 
