c2dd95e36f5e3442af20ff210c1b5f970b466ecf
paladox <paladox@users.noreply.github.com>
2019-01-13 00:49:12 +0000

    Update ManageWikiHooks.php

--- includes/ManageWikiHooks.php
+++ includes/ManageWikiHooks.php
@@ -188,42 +188,6 @@ class ManageWikiHooks {
 
 			ManageWikiCDB::changes( 'namespaces' );
 		}
-
-		if ( ManageWiki::checkSetup( 'namespaces' ) ) {
-			$dbw = wfGetDB( DB_MASTER, [], $wgCreateWikiDatabase );
-			$namespaces = $wgCanonicalNamespaceNames + [ 0 => '<Main>' ];
-
-			foreach ( $namespaces as $id => $name ) {
-				if ( $id < 0 ) {
-					// Don't include imaginary
-					continue;
-				}
-
-				$matchedNSKeys = array_keys( $wgNamespaceAliases, $id );
-				$nsAliases = [];
-
-				foreach ( $matchedNSKeys as $o => $n ) {
-					$nsAliases[] = $n;
-				}
-
-				// Wiki was just created, we can assume no entries in mw_namespaces
-				$dbw->insert(
-					'mw_namespaces',
-					[
-						'ns_dbname' => $dbname,
-						'ns_namespace_id' => (int)$id,
-						'ns_namespace_name' => (string)$name,
-						'ns_searchable' => (int)$wgNamespacesToBeSearchedDefault[$id],
-						'ns_subpages' => (int)$wgNamespacesWithSubpages[$id],
-						'ns_content' => (int)$wgContentNamespaces[$id],
-						'ns_protection' => ( is_array( $wgNamespaceProtection[$id] ) ) ? (string)$wgNamespaceProtection[$id][0] : (string)$wgNamespaceProtection[$id],
-						'ns_aliases' => (string)json_encode( $nsAliases ),
-						'ns_core' => ( $id < 1000 ) ? 1 : 0 // we assume less than < is "core", could do with smarter logic!
-					],
-					__METHOD__
-				);
-			}
-		}
 	}
 
 	public static function onCreateWikiDeletion( $dbw, $wiki ) {
