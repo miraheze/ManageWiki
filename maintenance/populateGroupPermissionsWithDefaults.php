c700533a8c95cf65636d748735cd36e37b716766
paladox <paladox@users.noreply.github.com>
2019-04-05 15:03:42 +0100

    Add populateGroupPermissionsWithDefaults script
    This script will allow us to quickly add default perms to wiki that are missing it.

---
+++ maintenance/populateGroupPermissionsWithDefaults.php
@@ -0,0 +1,46 @@
+<?php
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+class ManageWikiPopulatePermissionsWithDefaults extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+	}
+
+	function execute() {
+		global $wgCreateWikiDatabase, $wgManageWikiPermissionsDefaultPrivateGroup, $wgDBname;
+
+		if ( ManageWiki::checkSetup( 'permissions' ) ) {
+			$this->fatalError( 'Disable ManageWiki Permissions on this wiki.' );
+		}
+
+		$defaultGroups = array_diff( (array)ManageWikiPermissions::defaultGroups(), (array)$wgManageWikiPermissionsDefaultPrivateGroup );
+
+		$dbw = wfGetDB( DB_MASTER, [], $wgCreateWikiDatabase );
+
+		foreach ( $defaultGroups as $newgroup ) {
+			$grouparray = ManageWikiPermissions::defaultGroupPermissions( $newgroup );
+
+			$dbw->insert(
+				'mw_permissions',
+				[
+					'perm_dbname' => $wgDBname,
+					'perm_group' => $newgroup,
+					'perm_permissions' => json_encode( $grouparray['permissions'] ),
+					'perm_addgroups' => json_encode( $grouparray['addgroups'] ),
+					'perm_removegroups' => json_encode( $grouparray['removegroups'] )
+				],
+				__METHOD__
+			);
+		}
+
+		ManageWikiCDB::changes( 'permissions' );
+	}
+}
+
+$maintClass = 'v';
+require_once RUN_MAINTENANCE_IF_MAIN;
