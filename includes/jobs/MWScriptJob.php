ecbfd3a8221679a0d190b13a411ffcec5ad4696f
John Lewis <johnflewis93@gmail.com>
2019-05-14 22:57:41 +0100

    override limits for makeScriptCommand

--- includes/jobs/MWScriptJob.php
+++ includes/jobs/MWScriptJob.php
@@ -27,7 +27,7 @@ class MWScriptJob extends Job {
 		$result = Shell::makeScriptCommand(
 			$this->params['script'],
 			$scriptParams
-		)->execute()->getExitCode();
+		)->limits( [ 'memory' => 0, 'filesize' => 0 ] )->execute()->getExitCode();
 
 		if ( !$result ) {
 			wfDebugLog( 'ManageWiki', "MWScriptJob failure. Status {$result} running {$this->params['script']}" );
