fb099db77db3e093d80d24b154f700c409ae3402
paladox <paladox@users.noreply.github.com>
2019-06-26 18:03:21 +0100

    Update addNamespaces.php

--- maintenance/addNamespaces.php
+++ maintenance/addNamespaces.php
@@ -10,14 +10,14 @@ class ManageWikiAddNamespaces extends Maintenance {
 	public function __construct() {
 		parent::__construct();
 		$this->addOption( 'default', 'Wheather to add the namespace to the \'default\' db name (Defaults to wgDBname).' );
-		$this->addOption( 'id', 'The id for the namespace e.g 1.', true, true );
-		$this->addOption( 'name', 'Name of the namespace e.g Module.', true, true );
-		$this->addOption( 'searchable', 'Wheather the namespace is searchable.', true, true );
-		$this->addOption( 'subpages', 'Wheather the namespace has a subpage.', true, true );
-		$this->addOption( 'content', 'Wheather the namespace has content', true, true );
-		$this->addOption( 'contentmodel', 'Content model for the namespace.', true, true );
-		$this->addOption( 'protection', 'Wheather this namespace has protection.', true, true );
-		$this->addOption( 'core', 'Wheather to allow the namespaces to be renamed or not.', true, true );
+		$this->addOption( 'id', 'The namespace id e.g 1.', true, true );
+		$this->addOption( 'name', 'The name of the namespace e.g \'Module\'.', true, true );
+		$this->addOption( 'searchable', 'Whether the namespace is searchable.', true, true );
+		$this->addOption( 'subpages', 'Whether the namespace has a subpage.', true, true );
+		$this->addOption( 'content', 'Whether the namespace has content', true, true );
+		$this->addOption( 'contentmodel', 'The content model to use for the namespace.', true, true );
+		$this->addOption( 'protection', 'Whether this namespace has protection.', true, true );
+		$this->addOption( 'core', 'Whether to allow the namespaces to be renamed or not.', true, true );
 	}
 
 	public function execute() {
