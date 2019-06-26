c900f2b5133b20956ea3f0640b7264472c72e0b7
paladox <paladox@users.noreply.github.com>
2019-06-26 17:56:28 +0100

    Update addNamespaces.php

--- maintenance/addNamespaces.php
+++ maintenance/addNamespaces.php
@@ -8,32 +8,31 @@ require_once "$IP/maintenance/Maintenance.php";
 
 class ManageWikiAddNamespaces extends Maintenance {
 	public function __construct() {
-		global $wgCreateWikiDatabase;
 		parent::__construct();
 		$this->addOption( 'defaultdb', 'Wheather to add the namespace to the \'default\' db name (Defaults to wgDBname.' );
-		$this->addOption( 'nsid', 'The id for the namespace, e.g 1.', true, true );
-		$this->addOption( 'nsname', 'Name of the namespace, e.g Module.', true, true );
-		$this->addOption( 'nssearchable', 'Wheather the namespace is searchable.', true, true );
-		$this->addOption( 'nssubpages', 'Wheather the namespace has a subpage.', true, true );
-		$this->addOption( 'nscontent', 'Wheather the namespace has content', true, true );
-		$this->addOption( 'nscontentmodel', 'Content model for the namespace.', true, true );
-		$this->addOption( 'nsprotection', 'Wheather this namespace has protection.', true, true );
-		$this->addOption( 'nscore', 'Wheather to allow the namespace to be renamed or not.', true, true );
+		$this->addOption( 'id', 'The id for the namespace e.g 1.', true, true );
+		$this->addOption( 'name', 'Name of the namespace e.g Module.', true, true );
+		$this->addOption( 'searchable', 'Wheather the namespace is searchable.', true, true );
+		$this->addOption( 'subpages', 'Wheather the namespace has a subpage.', true, true );
+		$this->addOption( 'content', 'Wheather the namespace has content', true, true );
+		$this->addOption( 'contentmodel', 'Content model for the namespace.', true, true );
+		$this->addOption( 'protection', 'Wheather this namespace has protection.', true, true );
+		$this->addOption( 'core', 'Wheather to allow the namespaces to be renamed or not.', true, true );
 	}
 
 	public function execute() {
-		$id = (int)$this->getOption( 'nsid' );
-		$name = (string)$this->getOption( 'nsname' );
-		$search = (int)$this->getOption( 'nssearchable' );
-		$subpages = (int)$this->getOption( 'nssubpages' );
-		$protection = (string)$this->getOption( 'nsprotection' );
-		$content = (int)$this->getOption( 'nscontent' );
-		$model = (string)$this->getOption( 'nscontentmodel' );
-		$core = (int)$this->getOption( 'nscore' );
-		$model = (string)$this->getOption( 'nscontentmodel' );
+		$id = (int)$this->getOption( 'id' );
+		$name = (string)$this->getOption( 'name' );
+		$searchable = (int)$this->getOption( 'searchable' );
+		$subpages = (int)$this->getOption( 'subpages' );
+		$protection = (string)$this->getOption( 'protection' );
+		$content = (int)$this->getOption( 'content' );
+		$contentmodel = (string)$this->getOption( 'contentmodel' );
+		$core = (int)$this->getOption( 'core' );
+		$model = (string)$this->getOption( 'contentmodel' );
 		$dbname = $this->getOption( 'defaultdb' ) ? 'default' : null;
 
-		ManageWikiNamespaces::modifyNamespace( $id, $name, $search, $subpages, $protection, $content, $model, $core, [], [], $dbname );
+		ManageWikiNamespaces::modifyNamespace( $id, $name, $searchable, $subpages, $protection, $content, $contentmodel, $core, [], [], $dbname );
 
 		ManageWikiCDB::changes( 'namespaces' );
 	}
