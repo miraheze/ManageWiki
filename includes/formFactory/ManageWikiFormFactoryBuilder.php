cd15b8272a4293b96445c01c3ea7ea723ec5e13d
paladox <paladox@users.noreply.github.com>
2019-12-31 18:16:53 +0000

    Support an empty content model (#127)
    * Support an empty content model
    
    Sometimes the content model is managed by the extension it's self so no need for us to manage it.
    
    Fixes T4699
    
    * Update ManageWikiFormFactoryBuilder.php

--- includes/formFactory/ManageWikiFormFactoryBuilder.php
+++ includes/formFactory/ManageWikiFormFactoryBuilder.php
@@ -387,6 +387,7 @@ class ManageWikiFormFactoryBuilder {
 					'default' => ( $nsData ) ? $nsData->ns_content_model : 'wikitext',
 					'options' => array_merge( [
 						'CSS' => 'css',
+						'Extension Default' => '',
 						'JavaScript' => 'javascript',
 						'JSON' => 'json',
 						'Wikitext' => 'wikitext'
