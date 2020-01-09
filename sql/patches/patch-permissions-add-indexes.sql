b0b902623b8826928560e424e0c0e42d0f734e8d
paladox <paladox@users.noreply.github.com>
2020-01-09 18:20:40 +0000

    Update patch-permissions-add-indexes.sql

--- sql/patches/patch-permissions-add-indexes.sql
+++ sql/patches/patch-permissions-add-indexes.sql
@@ -1,6 +1,4 @@
--- Convert namespaces and permission table to use indexes for the db column
-ALTER TABLE /*$wgDBprefix*/mw_namespaces
-  ADD INDEX ns_dbname (ns_dbname);
+-- Convert permission table to use indexes for the db column
 
 ALTER TABLE /*$wgDBprefix*/mw_permissions
   ADD INDEX perm_dbname (perm_dbname);
