7d490ec1b725b155c333a96503a030073bd00542
paladox <paladox@users.noreply.github.com>
2020-04-15 20:28:19 +0100

    Add index to s_dbname for mw_settings

--- sql/mw_settings.sql
+++ sql/mw_settings.sql
@@ -1,5 +1,7 @@
 CREATE TABLE /*_*/mw_settings (
-  s_dbname VARCHAR(64) NOT NULL PRIMARY KEY,
+  s_dbname VARCHAR(64) NOT NULL,
   s_settings LONGTEXT NULL,
   s_extensions LONGTEXT NULL
 ) /*$wgDBTableOptions*/;
+
+CREATE INDEX /*i*/s_dbname ON /*_*/mw_settings (ss_dbname);
