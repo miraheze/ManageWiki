d01524176f6e305bbcdcf9fb6240eb3cb0be19aa
RhinosF1 <rhinosf1@gmail.com>
2020-06-26 13:29:51 +0100

    Create fixRCMaxAge.php

---
+++ maintenance/fixRCMaxAge.php
@@ -0,0 +1,38 @@
+<?php
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+class ManageWikiRemoveSettings extends Maintenance {
+
+	public function execute() {
+		global $wgCreateWikiDatabase, $wgDBname;
+
+		$dbw = wfGetDB( DB_MASTER, [], $wgCreateWikiDatabase );
+
+		$remoteWiki = RemoteWiki::newFromName( $wgDBname );
+		$settingsarray = $remoteWiki->getSettings();
+
+		if ( isset( $settingsarray['wgRCMaxAge' ) ] ) ) {
+			#something here
+		}
+
+		$settings = json_encode( $settingsarray );
+
+		$dbw->update( 'mw_settings',
+			[
+				's_settings' => $settings
+			],
+			[
+				's_dbname' => $wgDBname
+			],
+			__METHOD__
+		);
+	}
+}
+
+$maintClass = 'ManageWikiRemoveSettings';
+require_once RUN_MAINTENANCE_IF_MAIN;
