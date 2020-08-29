9f3690948327b97b76b584c63a2e39f3a2842b4f
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-08-28 19:02:58 -0500

    add perm_revoke

--- sql/mw_permissions.sql
+++ sql/mw_permissions.sql
@@ -2,6 +2,7 @@ CREATE TABLE /*_*/mw_permissions (
   `perm_dbname` VARCHAR(64) NOT NULL,
   `perm_group` VARCHAR(64) NOT NULL,
   `perm_permissions` LONGTEXT NOT NULL,
+  `perm_revoke` LONGTEXT NOT NULL,
   `perm_addgroups` LONGTEXT NOT NULL,
   `perm_removegroups` LONGTEXT NOT NULL,
   `perm_addgroupstoself` LONGTEXT NOT NULL,
