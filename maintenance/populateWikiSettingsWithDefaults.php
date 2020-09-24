b69c42dc780698cd338f469fe63f691c9480629d
paladox <paladox@users.noreply.github.com>
2020-09-24 22:24:50 +0100

    Create populateWikiSettingsWithDefaults.php (#184)
    * Create populateWikiSettingsWithDefaults.php
    
    * Update populateWikiSettingsWithDefaults.php
    
    * Update populateWikiSettingsWithDefaults.php

---
+++ maintenance/populateWikiSettingsWithDefaults.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__ . '/../../../maintenance/Maintenance.php';
+
+use MediaWiki\MediaWikiServices;
+
+class populateWikiSettingsWithDefaults extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function execute() {
+		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'createwiki' );
+		$dbw = wfGetDB( DB_MASTER, [], $config->get( 'CreateWikiDatabase' ) );
+
+		$dbw->insert(
+			'mw_settings',
+			[
+				's_dbname' => $config->get( 'DBname' ),
+				's_extensions' => json_encode( [] ),
+				's_settings' => json_encode( [] )
+			]
+		);
+
+		$this->recacheWikiJson( $config->get( 'DBname' ) );
+	}
+
+	private function recacheWikiJson( string $wiki ) {
+		$cWJ = new CreateWikiJson( $wiki );
+		$cWJ->resetWiki();
+	}
+}
+
+$maintClass = 'populateWikiSettingsWithDefaults';
+require_once RUN_MAINTENANCE_IF_MAIN;
