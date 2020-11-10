bd904d22780056959fd9a69d110f3cbb8e5d0539
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-11-10 11:10:52 -0700

    Add preferences type to ManageWikiSettings (#193)
    * Add preferences type
    
    * Remove if statements from foreach
    
    * Finshed
    
    * Add email preferences
    
    * Blacklist echo-subscriptions preferences

--- includes/formFactory/ManageWikiFormFactoryBuilder.php
+++ includes/formFactory/ManageWikiFormFactoryBuilder.php
@@ -328,6 +328,97 @@ class ManageWikiFormFactoryBuilder {
 							'type' => 'namespacesmultiselect',
 							'default' => $setList[$name] ?? $set['overridedefault']
 						];
+						break;	
+					case 'preferences':
+						$preferences = [];
+						$excludedPrefs = [];
+						$allPreferences = MediaWikiServices::getInstance()->getUserOptionsLookup()->getDefaultOptions();
+						
+						
+						// Don't show preferences hidden by configuratiom
+						if( !$config->get( 'AllowUserCssPrefs' ) ) {
+							$excludedPrefs[] = 'underline';
+							$excludedPrefs[] = 'editfont';
+						}
+							
+						if( $config->get( 'DisableLangConversion' ) ) {
+							$excludedPrefs[] = 'variant';
+						} else {
+							foreach( preg_grep( '/variant-[A-Za-z0-9]/', array_keys( $allPreferences ) ) as $pref => $value ) {
+								$excludedPrefs[] = array_keys( $allPreferences )[$pref];
+							}
+						}
+							
+						if( $config->get( 'ForceHTTPS' ) || !$config->get( 'SecureLogin' ) ) {
+							$excludedPrefs[] = 'prefershttps';
+						}
+							
+						if( !$config->get( 'RCShowWatchingUsers' ) ) {
+							$excludedPrefs[] = 'shownumberswatching';
+						}
+							
+						if( !$config->get( 'RCWatchCategoryMembership' ) ) {
+							$excludedPrefs[] = 'hidecategorization';
+							$excludedPrefs[] = 'watchlisthidecategorization';
+						}
+							
+						if( !$config->get( 'SearchMatchRedirectPreference' ) ) {
+							$excludedPrefs[] = 'search-match-redirect';
+						}
+						
+						if ( !$config->get( 'EnableEmail' ) ) {
+							if ( !$config->get( 'AllowRequiringEmailForResets' ) ) {
+								$excludedPrefs[] = 'requireemail';
+							}
+							if ( !$config->get( 'EnableUserEmail' ) ) {
+								$excludedPrefs[] = 'disablemail';
+								$excludedPrefs[] = 'email-allow-new-users';
+								$excludedPrefs[] = 'ccmeonemails';
+								if ( !$this->options->get( 'EnableUserEmailBlacklist' ) ) {
+									$excludedPrefs[] = 'EnableUserEmailBlacklist';
+								}
+							}
+							if ( !$config->get( 'EnotifWatchlist' ) ) {
+								$excludedPrefs[] = 'enotifwatchlistpages';
+							}
+							if ( !$config->get( 'EnotifUserTalk' ) ) {
+								$excludedPrefs[] = 'enotifusertalkpages';
+							}
+							if (!$config->get( 'EnotifUserTalk' ) && !$config->get( 'EnotifWatchlist' ) ) {
+								if ( !$config->get( 'EnotifMinorEdits' ) ) {
+									$excludedPrefs[] = 'enotifminoredits';
+								}
+								if ( !$config->get( 'EnotifRevealEditorAddress' ) ) {
+									$excludedPrefs[] = 'enotifrevealaddr';
+								}
+							}
+						}
+						
+						// Never show searchNs* prefs
+						foreach( preg_grep( '/searchNs[0-9]/', array_keys( $allPreferences ) ) as $pref => $value ) {
+							$excludedPrefs[] = array_keys( $allPreferences )[$pref];
+						}
+						
+						// Blacklist echo-subscriptions preferences
+						foreach( preg_grep( '/echo-subscriptions-(?s).*/', array_keys( $allPreferences ) ) as $pref => $value ) {
+							$excludedPrefs[] = array_keys( $allPreferences )[$pref];
+						}
+						
+						foreach( $allPreferences as $preference => $val ) {
+							if ( !in_array( $preference, $excludedPrefs ) ) {
+								$preferences[$preference] = $preference;
+							}
+						}
+						
+						$configs = [
+							'type' => 'multiselect',
+							'options' => $preferences,
+							'default' => $setList[$name] ?? $set['overridedefault']
+						];
+						
+						if ( !$disabled ) {
+							$configs['dropdown'] = true;
+						}
 						break;
 					case 'skin':
   						$enabledSkins = MediaWikiServices::getInstance()->getSkinFactory()->getSkinNames();
