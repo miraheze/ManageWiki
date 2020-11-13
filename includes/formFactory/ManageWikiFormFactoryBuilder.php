d1874b69805cff8c523f5ac2c289aee4c43db7e7
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-11-13 11:02:30 -0700

    Simplify

--- includes/formFactory/ManageWikiFormFactoryBuilder.php
+++ includes/formFactory/ManageWikiFormFactoryBuilder.php
@@ -633,13 +633,7 @@ class ManageWikiFormFactoryBuilder {
 
 				if ( $add && ( $a['main'] && $name == 'namespace' || $a['talk'] && $name == 'namespacetalk' ) && ( !in_array( $id, (array)$a['blacklisted'] ) ) ) {
 					
-					if ( is_array( $a['overridedefault'] ) ) {
-						if ( in_array( $id, array_keys( $a['overridedefault'] ) ) ) {
-							$a['overridedefault'] = $a['overridedefault'][$id];
-						} else {
-							$a['overridedefault'] = $a['overridedefault']['default'];
-						}
-					}
+					$a['overridedefault'] = $a['overridedefault'][$id] ?? $a['overridedefault']['default'];
 
 					$formDescriptor["$key-$name"] = [
 						'label' => $a['name'],
