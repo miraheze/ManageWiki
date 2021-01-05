487bfa034b1265ebfb180ed6087dd8a36a3eea89
paladox <paladox@users.noreply.github.com>
2021-01-05 18:42:39 +0000

    Add support for removing settings (#211)
    * Add support for removing settings
    
    * Update populateWikiSettings.php

--- maintenance/populateWikiSettings.php
+++ maintenance/populateWikiSettings.php
@@ -6,14 +6,25 @@ if ( $IP === false ) {
 }
 require_once "$IP/maintenance/Maintenance.php";
 
+use MediaWiki\MediaWikiServices;
+
 class ManageWikiPopulateSettings extends Maintenance {
 	public function __construct() {
 		parent::__construct();
 		$this->addOption( 'wgsetting', 'The $wg setting minus $.', true, true );
 		$this->addOption( 'sourcelist', 'File in format of "wiki|value" for the $wg setting above.', true, true );
+		$this->addOption( 'remove', 'Removes setting listed with --wgsetting.' );
 	}
 
 	public function execute() {
+		if ( (bool)$this->getOption( 'remove' ) ) {
+			$dbName = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'managewiki' )->get( 'DBname' );
+			$mwSettings = new ManageWikiSettings( $dbName );
+			$mwSettings->remove( [ $this->getOption( 'wgsetting' ) ] );
+			$mwSettings->commit();
+			return;
+		}
+
 		$settingsource = file( $this->getOption( 'sourcelist' ) );
 
 		foreach ( $settingsource as $input ) {
