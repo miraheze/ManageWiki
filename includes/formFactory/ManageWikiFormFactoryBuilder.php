7c93000d8920b5f64428690b4f26c72c3a6f5015
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-01-06 10:21:22 -0700

    Update ManageWikiFormFactoryBuilder.php

--- includes/formFactory/ManageWikiFormFactoryBuilder.php
+++ includes/formFactory/ManageWikiFormFactoryBuilder.php
@@ -263,7 +263,7 @@ class ManageWikiFormFactoryBuilder {
 				switch ( $set['type'] ) {
 					case 'databases':
 						$configs = [
-							'type' => HTMLAutoCompleteSelectFieldWithOOUI::class,
+							'class' => HTMLAutoCompleteSelectFieldWithOOUI::class,
 							'default' => $setList[$name] ?? $set['overridedefault'],
 							'require-match' => true
 						];
