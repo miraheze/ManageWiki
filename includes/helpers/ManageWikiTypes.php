26763299980feea1761e0e07face16bbb23ce236
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-02-28 14:07:50 -0700

    Update ManageWikiTypes.php

--- includes/helpers/ManageWikiTypes.php
+++ includes/helpers/ManageWikiTypes.php
@@ -16,7 +16,7 @@ class ManageWikiTypes {
 			case 'database':
 				$configs = [
 					'class' => HTMLAutoCompleteSelectFieldWithOOUI::class,
-					'default' => $value ?? $options['overridedefault'],
+					'default' => $value ?: $options['overridedefault'],
 					'require-match' => true
 				];
 
