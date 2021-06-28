d34e93e99f78fe7dc4805bd4ea7e586f1c0a2656
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-06-28 16:47:03 -0600

    Update ManageWikiFormFactoryBuilder.php

--- includes/formFactory/ManageWikiFormFactoryBuilder.php
+++ includes/formFactory/ManageWikiFormFactoryBuilder.php
@@ -206,7 +206,7 @@ class ManageWikiFormFactoryBuilder {
 			),
 		true );
 
-		$credits = array_merge( $reg->readFromQueue( $queue )['credits'], $config->get( 'ExtensionCredits' ) );
+		$credits = array_merge( $reg->readFromQueue( $queue )['credits'], array_values( array_merge(...array_values( $config->get( 'ExtensionCredits' ) ) ) ) );
 
 		$formDescriptor = [];
 
