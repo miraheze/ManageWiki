6754e3c26d2bcfa706554c33a1b1f016d5c89b6b
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-07-18 20:59:33 -0600

    Update ManageWikiOOUIForm.php

--- includes/ManageWikiOOUIForm.php
+++ includes/ManageWikiOOUIForm.php
@@ -124,7 +124,7 @@ class ManageWikiOOUIForm extends OOUIHTMLForm {
 		$html = new OOUI\TextInputWidget( [
 			'infusable' => true,
 			'placeholder' => wfMessage( 'managewiki-label-reason' )->text(),
-			'name' => 'wpreason'
+			'name' => 'wpreason',
 			'framed' => true,
 			'required' => true,
 		] );
