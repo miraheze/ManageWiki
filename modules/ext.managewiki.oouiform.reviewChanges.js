9fc6a33328beb801461908cd57c68833bb9cd717
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-07-26 16:23:58 -0600

    Update ext.managewiki.oouiform.reviewChanges.js

--- modules/ext.managewiki.oouiform.reviewChanges.js
+++ modules/ext.managewiki.oouiform.reviewChanges.js
@@ -1,71 +1,71 @@
 ( function () {
 	$( function () {
-		function ProcessDialog(config) {
-			ProcessDialog.super.call(this, config);
+		function ProcessDialog( config ) {
+			ProcessDialog.super.call( this, config );
 		}
 
-		OO.inheritClass(ProcessDialog, OO.ui.ProcessDialog);
+		OO.inheritClass( ProcessDialog, OO.ui.ProcessDialog );
 
 		ProcessDialog.static.name = 'managewiki-review';
 
 		ProcessDialog.static.title = mw.msg( 'managewiki-review-title' );
 
-		ProcessDialog.static.actions = [{
+		ProcessDialog.static.actions = [ {
 			label: mw.msg( 'managewiki-review-close' ),
 			flags: 'safe'
-		}];
+		} ];
 
-		ProcessDialog.prototype.initialize = function() {
-			ProcessDialog.super.prototype.initialize.apply(this, arguments);
+		ProcessDialog.prototype.initialize = function () {
+			ProcessDialog.super.prototype.initialize.apply( this, arguments );
 
-			this.content = new OO.ui.PanelLayout({
+			this.content = new OO.ui.PanelLayout( {
 				padded: true,
 				expanded: false
-			});
+			} );
 
 			var dialog = this;
-			$('#managewiki-review-changes').click( function() {
-				dialog.content.$element.html('');
-				$('#managewiki-form :input').each(function() {
-					if (this.type == 'checkbox' && this.defaultChecked != undefined && this.defaultChecked != this.checked) {
-						dialog.content.$element.append('<b>' + this.name.replace('wp', '').replace('[]', '[' + this.value + ']') + '</b>: ' + this.defaultChecked + ' -> ' + this.checked + '<br />');
-					} else if (this.defaultValue != undefined && this.defaultValue != this.value) {
-						dialog.content.$element.append('<b>' + this.name.replace('wp', '') + '</b>: ' + this.defaultValue + ' -> ' + this.value + '<br />');
+			$( '#managewiki-review-changes' ).click( function () {
+				dialog.content.$element.html( '' );
+				$( '#managewiki-form :input' ).each( function () {
+					if ( this.type == 'checkbox' && this.defaultChecked != undefined && this.defaultChecked != this.checked ) {
+						dialog.content.$element.append( '<b>' + this.name.replace( 'wp', '' ).replace( '[]', '[' + this.value + ']' ) + '</b>: ' + this.defaultChecked + ' -> ' + this.checked + '<br />' );
+					} else if ( this.defaultValue != undefined && this.defaultValue != this.value ) {
+						dialog.content.$element.append( '<b>' + this.name.replace( 'wp', '' ) + '</b>: ' + this.defaultValue + ' -> ' + this.value + '<br />' );
 					}
-				});
+				} );
 
-				dialog.$body.append(dialog.content.$element);
-			});
+				dialog.$body.append( dialog.content.$element );
+			} );
 		};
 
-		ProcessDialog.prototype.getActionProcess = function(action) {
+		ProcessDialog.prototype.getActionProcess = function ( action ) {
 			var dialog = this;
-			if (action) {
-				return new OO.ui.Process(function() {
-					dialog.close({
+			if ( action ) {
+				return new OO.ui.Process( function () {
+					dialog.close( {
 						action: action
-					});
-				});
+					} );
+				} );
 			}
 
-			return ProcessDialog.super.prototype.getActionProcess.call(this, action);
+			return ProcessDialog.super.prototype.getActionProcess.call( this, action );
 		};
 
-		ProcessDialog.prototype.getBodyHeight = function() {
-			return this.content.$element.outerHeight(true);
+		ProcessDialog.prototype.getBodyHeight = function () {
+			return this.content.$element.outerHeight( true );
 		};
 
 		var windowManager = new OO.ui.WindowManager();
-		$(document.body).append(windowManager.$element);
+		$( document.body ).append( windowManager.$element );
 
-		var processDialog = new ProcessDialog({
+		var processDialog = new ProcessDialog( {
 			size: 'medium'
-		});
+		} );
 
-		windowManager.addWindows([processDialog]);
+		windowManager.addWindows( [processDialog] );
 
-		$('#managewiki-review-changes').click( function() {
-			windowManager.openWindow(processDialog);
-		});
+		$( '#managewiki-review-changes' ).click( function () {
+			windowManager.openWindow( processDialog );
+		} );
 	} );
 }() );
