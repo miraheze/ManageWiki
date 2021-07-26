6769caab9808514459bd6853b18119d24657325d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-07-26 11:11:21 -0600

    Create ext.managewiki.oouiform.reviewChanges.js

---
+++ modules/ext.managewiki.oouiform.reviewChanges.js
@@ -0,0 +1,66 @@
+(function() {
+	$(function() {
+		function ProcessDialog(config) {
+			ProcessDialog.super.call(this, config);
+		}
+
+		OO.inheritClass(ProcessDialog, OO.ui.ProcessDialog);
+
+		ProcessDialog.static.name = 'managewiki-review';
+
+		ProcessDialog.static.title = 'Review changes';
+
+		ProcessDialog.static.actions = [{
+			label: 'Close',
+			flags: 'safe'
+		}];
+
+		ProcessDialog.prototype.initialize = function() {
+			ProcessDialog.super.prototype.initialize.apply(this, arguments);
+
+			this.content = new OO.ui.PanelLayout({
+				padded: true,
+				expanded: false
+			});
+
+			var $this = this;
+			$('#managewiki-form :input').each(function() {
+				if (this.type === 'checkbox' && this.defaultChecked != undefined && this.defaultChecked != this.checked) {
+					$this.content.$element.append('<b>' + this.name.replace('wp', '').replace('[]', '[' + this.value + ']') + '</b>: ' + this.defaultChecked + ' —> ' + this.checked + '<br />');
+				} else if (this.defaultValue != undefined && this.defaultValue != this.value) {
+					$this.content.$element.append('<b>' + this.name.replace('wp', '') + '</b>: ' + this.defaultValue + ' —> ' + this.value + '<br />');
+				}
+			});
+
+			this.$body.append(this.content.$element);
+		};
+
+		ProcessDialog.prototype.getActionProcess = function(action) {
+			var dialog = this;
+			if (action) {
+				return new OO.ui.Process(function() {
+					dialog.close({
+						action: action
+					});
+				});
+			}
+
+			return ProcessDialog.super.prototype.getActionProcess.call(this, action);
+		};
+
+		ProcessDialog.prototype.getBodyHeight = function() {
+			return this.content.$element.outerHeight(true);
+		};
+
+		var windowManager = new OO.ui.WindowManager();
+		$(document.body).append(windowManager.$element);
+
+		var processDialog = new ProcessDialog({
+			size: 'medium'
+		});
+
+		windowManager.addWindows([processDialog]);
+
+		windowManager.openWindow(processDialog);
+	});
+}());
