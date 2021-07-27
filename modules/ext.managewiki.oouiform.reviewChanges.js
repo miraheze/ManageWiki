2ed7d3797b0c8db7598fdbaefc3d9053d6878f62
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-07-27 17:02:52 -0600

    Update ext.managewiki.oouiform.reviewChanges.js

--- modules/ext.managewiki.oouiform.reviewChanges.js
+++ modules/ext.managewiki.oouiform.reviewChanges.js
@@ -31,6 +31,7 @@
 						dialog.content.$element.append( '<li><b>' + this.name.replace( 'wp', '' ).replace( '[]', '[' + this.value + ']' ) + '</b> ' + 'was <i>' + ( this.checked == true ? 'enabled' : 'disabled' ) + '</i></li>' );
 					} else if ( this.defaultValue != undefined && this.defaultValue != this.value ) {
 						dialog.content.$element.append( '<li><b>' + this.name.replace( 'wp', '' ) + '</b> was changed from <i>' + ( this.defaultValue ? this.defaultValue : '&lt;none&gt;' ) + '</i> to <i>' + ( this.value ? this.value : '&lt;none&gt;' ) + '</i></li>' );
+						dialog.content.$element.append( '<li>' + $( $( this.name ).parents( 'fieldset' ).contents()[0] ).text() ) + '</li>' );
 					}
 				} );
 
