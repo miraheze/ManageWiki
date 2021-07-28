e6d777911479e3600133ab49ed2b25012be5c24b
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-07-27 18:54:22 -0600

    Update ext.managewiki.oouiform.reviewChanges.js

--- modules/ext.managewiki.oouiform.reviewChanges.js
+++ modules/ext.managewiki.oouiform.reviewChanges.js
@@ -30,7 +30,7 @@
 					if ( this.type == 'checkbox' && this.defaultChecked != undefined && this.defaultChecked != this.checked ) {
 						dialog.content.$element.append( '<li><b>' + this.name.replace( 'wp', '' ).replace( /-namespace|-namespacetalk|ext-|set-/, '' ).replace( '[]', '[' + this.value + ']' ) + ' (' + $( $( this ).parents( 'fieldset' ).contents()[0] ).text() + ')</b> was <i>' + ( this.checked == true ? 'enabled' : 'disabled' ) + '</i></li>' );
 					} else if ( this.defaultValue != undefined && this.defaultValue != this.value ) {
-						var info = this.id ?
+						var popup =
 							new OO.ui.PopupButtonWidget( {
 								icon: 'info',
 								framed: false,
@@ -43,9 +43,8 @@
 									padded: true,
 									align: 'forwards'
 								}
-							} ).$element
-						: '';
-						dialog.content.$element.append( '<li><b>' + this.name.replace( 'wp', '' ).replace( /-namespace|-namespacetalk|ext-|set-/, '' ) + ' (' + $( $( this ).parents( 'fieldset' ).contents()[0] ).text() + ')</b>' + info + ' was changed from <i>' + ( this.defaultValue ? this.defaultValue : '&lt;none&gt;' ) + '</i> to <i>' + ( this.value ? this.value : '&lt;none&gt;' ) + '</i></li>' );
+							} );
+						dialog.content.$element.append( '<li><b>' + this.name.replace( 'wp', '' ).replace( /-namespace|-namespacetalk|ext-|set-/, '' ) + ' (' + $( $( this ).parents( 'fieldset' ).contents()[0] ).text() + ')</b>' + popup.$element + ' was changed from <i>' + ( this.defaultValue ? this.defaultValue : '&lt;none&gt;' ) + '</i> to <i>' + ( this.value ? this.value : '&lt;none&gt;' ) + '</i></li>' );
 					}
 				} );
 
