98a347918807b78b2fa4401e40e6c3e0fe71f9d1
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-01 22:24:14 -0600

    Update config.php

--- .phan/config.php
+++ .phan/config.php
@@ -4,12 +4,14 @@ $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.
 
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'], [
+		'CreateWiki',
 		'mediawiki',
 	]
 );
 
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'], [
+		'CreateWiki',
 		'mediawiki',
 	]
 );
