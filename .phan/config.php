56ac48bf2d7b78b6dd314cf061eb4fa6574d692b
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-01 22:03:25 -0600

    Create config.php

---
+++ .phan/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+$cfg['suppress_issue_types'] = array_merge(
+	$cfg['suppress_issue_types'], [
+		'PhanPluginDuplicateAdjacentStatement',
+	]
+);
+
+$cfg['scalar_implicit_cast'] = true;
+
+return $cfg;
