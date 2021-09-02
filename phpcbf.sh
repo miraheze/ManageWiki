28222d0cc9b81de7b578bdcd8717789b108afdd2
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-02 13:18:34 -0600

    Update phpcbf.sh

--- phpcbf.sh
+++ phpcbf.sh
@@ -1,7 +1,7 @@
 # Force phpcbf to exit with zero
 # See https://github.com/squizlabs/PHP_CodeSniffer/issues/1818#issuecomment-354420927
 
-root=$( dirname $0 )/..
+root=$( dirname $0 )
 
 $root/vendor/bin/phpcbf
 
