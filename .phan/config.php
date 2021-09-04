8c68a5688095b8d6f4aca524c76f47232376f961
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-03 22:43:15 -0600

    Update config.php

--- .phan/config.php
+++ .phan/config.php
@@ -6,14 +6,12 @@ $cfg['target_php_version'] = '7.3';
 
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'], [
-		'CreateWiki',
 		'mediawiki',
 	]
 );
 
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'], [
-		'CreateWiki',
 		'mediawiki',
 	]
 );
