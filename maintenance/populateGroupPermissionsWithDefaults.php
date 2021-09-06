9dda9f230d4fa2af43857c3db1434fc1523ac62a
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-06 13:09:17 -0600

    Update populateGroupPermissionsWithDefaults.php

--- maintenance/populateGroupPermissionsWithDefaults.php
+++ maintenance/populateGroupPermissionsWithDefaults.php
@@ -8,7 +8,7 @@ require_once "$IP/maintenance/Maintenance.php";
 
 use MediaWiki\MediaWikiServices;
 
-class ManageWikiPopulatePermissionsWithDefaults extends Maintenance {
+class PopulateGroupPermissionsWithDefaults extends Maintenance {
 	public function __construct() {
 		parent::__construct();
 		$this->addOption( 'overwrite', 'This overwrites perms to reset them back to the default.', false, false );
