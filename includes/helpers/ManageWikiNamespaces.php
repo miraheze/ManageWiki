978e36b5eab22ae57e49a3c75517703044289349
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-21 19:10:47 -0700

    ManageWikiNamespacesBlacklistedNames: case insensitive

--- includes/helpers/ManageWikiNamespaces.php
+++ includes/helpers/ManageWikiNamespaces.php
@@ -92,7 +92,8 @@ class ManageWikiNamespaces {
 	 * @param bool $maintainPrefix|false
 	 */
 	public function modify( int $id, array $data, bool $maintainPrefix = false ) {
-		if ( in_array( $data['name'], $this->config->get( 'ManageWikiNamespacesBlacklistedNames' ) ) ) {
+		$blacklist = array_map( 'strtolower', $this->config->get( 'ManageWikiNamespacesBlacklistedNames' ) );
+		if ( in_array( strtolower( $data['name'] ), $blacklist ) ) {
 			$this->errors[] = [
 				'managewiki-error-disallowednamespace' => [
 					$data['name']
