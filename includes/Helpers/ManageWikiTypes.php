ca9e3361639dff4b4753392166e6d0017aa6144c
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-12 16:04:00 -0600

    Update ManageWikiTypes.php

--- includes/Helpers/ManageWikiTypes.php
+++ includes/Helpers/ManageWikiTypes.php
@@ -334,7 +334,7 @@ class ManageWikiTypes {
 				$configs = [
 					'type' => 'usersmultiselect',
 					'exists' => true,
-					'default' => implode( "\n", $value ?? $options['overridedefault'] )
+					'default' => $value ?? $options['overridedefault']
 				];
 				break;
 			case 'usergroups':
@@ -381,7 +381,7 @@ class ManageWikiTypes {
 				$configs = [
 					'type' => 'titlesmultiselect',
 					'exists' => $options['exists'] ?? true,
-					'default' => implode( "\n", $value ?? $options['overridedefault'] ),
+					'default' => $value ?? $options['overridedefault'],
 					'required' => false
 				];
 				break;
