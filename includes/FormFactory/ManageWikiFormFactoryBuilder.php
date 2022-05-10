0301b3a71de9fbc4c76a45568123445a6597cdbd
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-05-10 15:04:57 -0600

    Fix retrieving default for autopromote groups multiselect field

--- includes/FormFactory/ManageWikiFormFactoryBuilder.php
+++ includes/FormFactory/ManageWikiFormFactoryBuilder.php
@@ -689,7 +689,7 @@ class ManageWikiFormFactoryBuilder {
 		$aPArray = [];
 		foreach ( (array)$aP as $element ) {
 			if ( is_array( $element ) ) {
-				$aPArray[$element[0]] = $element[1];
+				$aPArray[$element[0]] = array_slice( $element, 1 );
 			}
 		}
 
@@ -1187,7 +1187,7 @@ class ManageWikiFormFactoryBuilder {
 		$aE = $formData['enable'];
 
 		$aPBuild = $aE ? [
-				$formData['conds']
+			$formData['conds']
 		] : [];
 
 		if ( count( $aPBuild ) != 0 ) {
