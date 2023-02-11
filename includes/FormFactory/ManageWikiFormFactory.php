7208fe5d248465cb92c410741f01ebed073760a9
paladox <paladox@users.noreply.github.com>
2023-02-11 19:42:54 +0000

    Fix displaying the error in the users language
    It was instead displaying in the wikis main language and not the language the user has set in their preference.

--- includes/FormFactory/ManageWikiFormFactory.php
+++ includes/FormFactory/ManageWikiFormFactory.php
@@ -96,7 +96,7 @@ class ManageWikiFormFactory {
 			$errorOut = [];
 			foreach ( $mwReturn as $errors ) {
 				foreach ( $errors as $msg => $params ) {
-					$errorOut[] = wfMessage( $msg, $params )->inContentLanguage()->escaped();
+					$errorOut[] = wfMessage( $msg, $params )->escaped();
 				}
 			}
 
