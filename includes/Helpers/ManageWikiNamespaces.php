46e42edd1e523560f6a8ed294e4e02d96836bf58
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-14 12:35:14 -0700

    Update ManageWikiNamespaces.php

--- includes/Helpers/ManageWikiNamespaces.php
+++ includes/Helpers/ManageWikiNamespaces.php
@@ -44,7 +44,10 @@ class ManageWikiNamespaces {
 	public function __construct( string $wiki ) {
 		$this->wiki = $wiki;
 		$this->config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'managewiki' );
-		$this->dbw = wfGetDB( DB_PRIMARY, [], $this->config->get( 'CreateWikiDatabase' ) );
+
+		$this->dbw = MediaWikiServices::getInstance()->getDBLoadBalancerFactory()
+			->getMainLB( $this->config->get( 'CreateWikiDatabase' ) )
+			->getMaintenanceConnectionRef( DB_PRIMARY, [], $this->config->get( 'CreateWikiDatabase' ) );
 
 		$namespaces = $this->dbw->select(
 			'mw_namespaces',
