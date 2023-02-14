4bd4e05da6976e75a1a90113bc7a35dda729149c
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-14 12:26:47 -0700

    Replace deprecated `wfGetDB()`

--- includes/Helpers/ManageWikiPermissions.php
+++ includes/Helpers/ManageWikiPermissions.php
@@ -41,7 +41,9 @@ class ManageWikiPermissions {
 	public function __construct( string $wiki ) {
 		$this->wiki = $wiki;
 		$this->config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'managewiki' );
-		$this->dbw = wfGetDB( DB_PRIMARY, [], $this->config->get( 'CreateWikiDatabase' ) );
+		$this->dbw = MediaWikiServices::getInstance()->getDBLoadBalancerFactory()
+			->getMainLB( $this->config->get( 'CreateWikiDatabase' ) )
+			->getMaintenanceConnectionRef( DB_REPLICA, [], $this->config->get( 'CreateWikiDatabase' ) );
 
 		$perms = $this->dbw->select(
 			'mw_permissions',
