3f65654f41792d7d863f333ad38d6c8c71ee955c
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-04-24 18:24:16 -0600

    Pass $context

--- includes/Hooks/ManageWikiCoreFormSubmissionHook.php
+++ includes/Hooks/ManageWikiCoreFormSubmissionHook.php
@@ -2,14 +2,16 @@
 
 namespace Miraheze\ManageWiki\Hooks;
 
+use IContextSource;
 use Wikimedia\Rdbms\DBConnRef;
 
 interface ManageWikiCoreFormSubmissionHook {
 	/**
+	 * @param IContextSource $context
 	 * @param string $dbName
 	 * @param DBConnRef $dbw
 	 * @param array $formData
 	 * @return void
 	 */
-	public function onManageWikiCoreFormSubmission( $dbName, $dbw, $formData ): void;
+	public function onManageWikiCoreFormSubmission( $context, $dbName, $dbw, $formData ): void;
 }
