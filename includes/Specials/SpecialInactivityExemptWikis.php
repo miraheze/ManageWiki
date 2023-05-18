992f71723c4fa436b879ddf30d0c07a9ff1b0bb5
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-05-17 22:59:54 -0500

    Create SpecialInactivityExemptWikis.php
    From SpecialDeletedWikis.php code

---
+++ includes/Specials/SpecialInactivityExemptWikis.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Miraheze\ManageWiki\Specials;
+
+use Miraheze\ManageWiki\Helpers\ManageWikiInactiveExemptWikiPager;
+use IncludableSpecialPage;
+
+class SpecialInactivityExemptWikis extends IncludableSpecialPage {
+        public function __construct() {
+                parent::__construct( 'InactivityExemptWikis' );
+        }
+
+        public function execute( $par ) {
+                $this->setHeaders();
+                $this->outputHeader();
+
+                $pager = new ManageWikiInactiveExemptWikiPager( $this );
+
+                $this->getOutput()->addParserOutputContent( $pager->getFullOutput() );
+        }
+}
