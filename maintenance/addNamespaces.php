1b3b126188291fbbc9741e93b520ef7b1c0b91a2
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-01 10:45:26 -0700

    Update

--- maintenance/addNamespaces.php
+++ maintenance/addNamespaces.php
@@ -6,6 +6,7 @@ $IP = getenv( 'MW_INSTALL_PATH' );
 if ( $IP === false ) {
 	$IP = __DIR__ . '/../../..';
 }
+
 require_once "$IP/maintenance/Maintenance.php";
 
 use Maintenance;
@@ -15,6 +16,7 @@ use Miraheze\ManageWiki\Helpers\ManageWikiNamespaces;
 class AddNamespaces extends Maintenance {
 	public function __construct() {
 		parent::__construct();
+
 		$this->addOption( 'default', 'Wheather to add the namespace to the \'default\' db name (Defaults to wgDBname).' );
 		$this->addOption( 'id', 'The namespace id e.g 1.', true, true );
 		$this->addOption( 'name', 'The name of the namespace e.g \'Module\'.', true, true );
@@ -24,6 +26,8 @@ class AddNamespaces extends Maintenance {
 		$this->addOption( 'contentmodel', 'The content model to use for the namespace.', true, true );
 		$this->addOption( 'protection', 'Whether this namespace has protection.', true, true );
 		$this->addOption( 'core', 'Whether to allow the namespaces to be renamed or not.' );
+
+		$this->requireExtension( 'ManageWiki' );
 	}
 
 	public function execute() {
