da2c38332d5f43c4385d9fc3e4e11e19c65c3c90
Vũ Minh Nguyên <PiscesKaze@gmail.com>
2024-07-22 03:51:12 +0700

    Update ManageWikiAliases.php (#475)

--- ManageWikiAliases.php
+++ ManageWikiAliases.php
@@ -7,3 +7,9 @@ $specialPageAliases['en'] = [
 	'ManageWiki' => [ 'ManageWiki' ],
 	'ManageWikiDefaultPermissions' => [ 'ManageWikiDefaultPermissions' ],
 ];
+
+$specialPageAliases['vi'] = [
+	'DeletedWikis' => [ 'Wiki bị xóa' ],
+	'ManageWiki' => [ 'Quản lý wiki' ],
+	'ManageWikiDefaultPermissions' => [ 'Quản lý quyền mặc định của wiki' ],
+];
