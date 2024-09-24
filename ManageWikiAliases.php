d10fd4ec44313b007af868053f161dc956384afe
すずねーう <67305123+waki285@users.noreply.github.com>
2024-09-25 03:40:48 +0900

    Create an alias for special pages in Japanese (#476)
    Co-authored-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- ManageWikiAliases.php
+++ ManageWikiAliases.php
@@ -13,3 +13,9 @@ $specialPageAliases['vi'] = [
 	'ManageWiki' => [ 'Quản lý wiki' ],
 	'ManageWikiDefaultPermissions' => [ 'Quản lý quyền mặc định của wiki' ],
 ];
+
+$specialPageAliases['ja'] = [
+	'DeletedWikis' => [ '削除されたウィキ' ],
+	'ManageWiki' => [ 'ウィキ管理' ],
+	'ManageWikiDefaultPermissions' => [ 'ウィキ管理既定権限' ],
+];
