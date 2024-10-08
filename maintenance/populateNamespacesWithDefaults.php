c61f90e3c91640b306a8c4c82118d9d2cdbc339e
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-09 03:03:28 +0800

    Use CreateWikiDataFactory

--- maintenance/populateNamespacesWithDefaults.php
+++ maintenance/populateNamespacesWithDefaults.php
@@ -11,8 +11,6 @@ require_once "$IP/maintenance/Maintenance.php";
 
 use Maintenance;
 use MediaWiki\MainConfigNames;
-use Miraheze\CreateWiki\CreateWikiJson;
-use Miraheze\CreateWiki\CreateWikiPhp;
 use Miraheze\ManageWiki\Helpers\ManageWikiNamespaces;
 
 class PopulateNamespacesWithDefaults extends Maintenance {
@@ -57,19 +55,9 @@ class PopulateNamespacesWithDefaults extends Maintenance {
 				$mwNamespaces->commit();
 			}
 
-			if ( $this->getConfig()->get( 'CreateWikiUsePhpCache' ) ) {
-				$cWP = new CreateWikiPhp(
-					$this->getConfig()->get( MainConfigNames::DBname ),
-					$this->getServiceContainer()->get( 'CreateWikiHookRunner' )
-				);
-				$cWP->resetWiki();
-			} else {
-				$cWJ = new CreateWikiJson(
-					$this->getConfig()->get( MainConfigNames::DBname ),
-					$this->getServiceContainer()->get( 'CreateWikiHookRunner' )
-				);
-				$cWJ->resetWiki();
-			}
+			$dataFactory = $this->getServiceContainer()->get( 'CreateWikiDataFactory' );
+			$data = $dataFactory->newInstance( $this->getConfig()->get( MainConfigNames::DBname ) );
+			$data->resetWikiData();
 		}
 	}
 }
