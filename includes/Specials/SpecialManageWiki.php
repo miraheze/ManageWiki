9930c1c68a438748d11ad564c897be3a77806efd
MacFan4000 <paulfkeffer@gmail.com>
2025-02-04 10:57:10 -0500

    Update SpecialManageWiki.php

--- includes/Specials/SpecialManageWiki.php
+++ includes/Specials/SpecialManageWiki.php
@@ -162,7 +162,7 @@ class SpecialManageWiki extends SpecialPage {
 				$out->addHTML(
 					Html::errorBox( $this->msg( 'managewiki-error-nopermission-remote' )->escaped() )
 				);
-			} elseif ( !$this->getContext()->getUser()->isAllowed( 'managewiki-' . $module ) && !$isCentralWiki ) {
+			} elseif ( !$this->getContext()->getUser()->isAllowed( 'managewiki-' . $module ) && !($this->databaseUtils->isCurrentWikiCentral(); ) {
 				$out->addHTML(
 					Html::errorBox( $this->msg( 'managewiki-error-nopermission' )->escaped() )
 				);
