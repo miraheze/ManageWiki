dcf9f22fb0ad9b69196fe4cc06609cdb125befc7
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-02-12 19:31:50 -0700

    Fix

--- includes/Helpers/ManageWikiTypes.php
+++ includes/Helpers/ManageWikiTypes.php
@@ -35,7 +35,7 @@ class ManageWikiTypes {
 					'default' => $value ?? $options['overridedefault'],
 					'validation-callback' => static function ( $database ) use ( $config, $name ) {
 						if ( !in_array( $database, $config->get( 'LocalDatabases' ) ) ) {
-							return Status::newFatal( 'managewiki-invalid-database', $database, $name )->getMessage();
+							return wfMessage( 'managewiki-invalid-database', $database, $name );
 						}
 
 						return true;
@@ -245,7 +245,7 @@ class ManageWikiTypes {
 				}
 				break;
 			case 'skin':
-				$enabledSkins = MediaWikiServices::getInstance()->getSkinFactory()->getSkinNames();
+				$enabledSkins = MediaWikiServices::getInstance()->getSkinFactory()->getInstalledSkins();
 
 				unset( $enabledSkins['fallback'] );
 				unset( $enabledSkins['apioutput'] );
@@ -268,7 +268,7 @@ class ManageWikiTypes {
 				];
 				break;
 			case 'skins':
-				$enabledSkins = MediaWikiServices::getInstance()->getSkinFactory()->getSkinNames();
+				$enabledSkins = MediaWikiServices::getInstance()->getSkinFactory()->getInstalledSkins();
 
 				unset( $enabledSkins['fallback'] );
 				unset( $enabledSkins['apioutput'] );
