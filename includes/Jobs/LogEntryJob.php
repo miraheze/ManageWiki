181727b59f41da2e35c2f1bb7b07454b9c4d82a5
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-20 17:52:59 -0600

    Add Job to create log entries

---
+++ includes/Jobs/LogEntryJob.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Miraheze\ManageWiki\Jobs;
+
+use Job;
+use ManualLogEntry;
+use MediaWiki\Title\TitleFactory;
+use MediaWiki\User\User;
+use Wikimedia\Rdbms\IReadableDatabase;
+
+class LogEntryJob extends Job {
+
+	public const JOB_NAME = 'LogEntryJob';
+
+	private readonly string $logType;
+	private readonly string $action;
+	private readonly string $comment;
+	private readonly string $target;
+	private readonly array $logParams;
+
+	public function __construct(
+		array $params,
+		private readonly TitleFactory $titleFactory
+	) {
+		parent::__construct( self::JOB_NAME, $params );
+
+		$this->logType = $params['logType'];
+		$this->action = $params['action'];
+		$this->comment = $params['comment'];
+		$this->target = $params['target'];
+		$this->logParams = $params['logParams'];
+	}
+
+	public function run(): bool {
+		$title = $this->titleFactory->newFromText( $this->target );
+		if ( !$title ) {
+			return false;
+		}
+
+		$user = User::newSystemUser( 'ManageWiki', [ 'steal' => true ] );
+		if ( !$user ) {
+			return false;
+		}
+
+		$entry = new ManualLogEntry( $this->logType, $this->action );
+		$entry->setTarget( $title );
+		$entry->setComment( $this->comment );
+		$entry->setPerformer( $user );
+		$entry->setParameters( $this->logParams );
+
+		$logId = $entry->insert();
+		$entry->publish( $logId );
+
+		return true;
+	}
+}
