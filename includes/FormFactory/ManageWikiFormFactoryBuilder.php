a423223600ec461ca4edad5ff4d7dd71a31a5267
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-19 18:03:13 -0600

    +

--- includes/FormFactory/ManageWikiFormFactoryBuilder.php
+++ includes/FormFactory/ManageWikiFormFactoryBuilder.php
@@ -431,6 +431,7 @@ class ManageWikiFormFactoryBuilder {
 					'label' => ( $msgName->exists() ? $msgName->text() : $set['name'] ) . $varName,
 					'disabled' => $disabled,
 					'help' => $help,
+					'cssclass' => 'managewiki-infuse',
 					'section' => $set['section'],
 				] + $configs;
 			}
