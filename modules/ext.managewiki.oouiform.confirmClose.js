8f35236a1851328c0b86953e2fc4955acce4bdb3
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-21 19:52:12 -0600

    Improvements

--- modules/ext.managewiki.oouiform.confirmClose.js
+++ modules/ext.managewiki.oouiform.confirmClose.js
@@ -25,14 +25,41 @@
 
 			$fields = $( '#managewiki-form :input[name]:not( #managewiki-submit-reason :input[name] )' );
 			for ( i = 0; i < $fields.length; i++ ) {
-				if ( $fields[ i ].defaultChecked !== undefined && $fields[ i ].type === 'checkbox' && $fields[ i ].defaultChecked !== $fields[ i ].checked ) {
+				if (
+					$fields[ i ].defaultChecked !== undefined &&
+					$fields[ i ].type === 'checkbox' &&
+					$fields[ i ].defaultChecked !== $fields[ i ].checked
+				) {
 					return true;
-				} else if ( $fields[ i ].defaultValue !== undefined && $fields[ i ].defaultValue !== $fields[ i ].value ) {
+				} else if (
+					$fields[ i ].defaultValue !== undefined &&
+					$fields[ i ].defaultValue !== $fields[ i ].value
+				) {
 					return true;
 				}
 			}
 
 			return false;
+		}
+
+		// Check if a submit reason was entered
+		function hasSubmitReason() {
+			const $reason = $( '#managewiki-submit-reason' ).find( ':input[name]' );
+			for ( let i = 0; i < $reason.length; i++ ) {
+				if ( $reason[ i ].value.trim() !== '' ) {
+					return true;
+				}
+			}
+			return false;
+		}
+
+		// Determine if the save button should be enabled
+		function updateSaveButtonState() {
+			const changed = isManageWikiChanged();
+			const reasonFilled = hasSubmitReason();
+			// eslint-disable-next-line no-jquery/no-class-state
+			const isCreateNamespace = $( 'body' ).hasClass( 'ext-managewiki-create-namespace' );
+			saveButton.setDisabled( !( changed || ( isCreateNamespace && reasonFilled ) ) );
 		}
 
 		// Store the initial number of children of cloners for later use, as an equivalent of
@@ -43,29 +70,23 @@
 			}
 		} );
 
-		// Allow creating a new namespace without making any changes to the form
-		// eslint-disable-next-line no-jquery/no-class-state
-		if ( !$( 'body' ).hasClass( 'ext-managewiki-create-namespace' ) ) {
-			saveButton = OO.ui.infuse( $( '#managewiki-submit' ) );
+		saveButton = OO.ui.infuse( $( '#managewiki-submit' ) );
 
-			// Disable the save button unless settings have changed
-			// Check if settings have been changed before JS has finished loading
-			saveButton.setDisabled( !isManageWikiChanged() );
+		// Disable the save button unless settings have changed
+		// Check if settings have been changed before JS has finished loading
+		updateSaveButtonState();
 
-			// Attach capturing event handlers to the document, to catch events inside OOUI dropdowns:
-			// * Use capture because OO.ui.SelectWidget also does, and it stops event propagation,
-			//   so the event is not fired on descendant elements
-			// * Attach to the document because the dropdowns are in the .oo-ui-defaultOverlay element
-			//   (and it doesn't exist yet at this point, so we can't attach them to it)
-			[ 'change', 'keyup', 'mouseup' ].forEach( ( eventType ) => {
-				document.addEventListener( eventType, () => {
-					// Make sure SelectWidget's event handlers run first
-					setTimeout( () => {
-						saveButton.setDisabled( !isManageWikiChanged() );
-					} );
-				}, true );
-			} );
-		}
+		// Attach capturing event handlers to the document, to catch events inside OOUI dropdowns:
+		// * Use capture because OO.ui.SelectWidget also does, and it stops event propagation,
+		//   so the event is not fired on descendant elements
+		// * Attach to the document because the dropdowns are in the .oo-ui-defaultOverlay element
+		//   (and it doesn't exist yet at this point, so we can't attach them to it)
+		[ 'change', 'keyup', 'mouseup' ].forEach( ( eventType ) => {
+			document.addEventListener( eventType, () => {
+				// Make sure SelectWidget's event handlers run first
+				setTimeout( updateSaveButtonState );
+			}, true );
+		} );
 
 		// Set up a message to notify users if they try to leave the page without
 		// saving.
