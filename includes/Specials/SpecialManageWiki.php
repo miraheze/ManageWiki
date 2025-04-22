e19bba26047947d1fb7611a330bb88238c78d471
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-22 15:32:19 -0600

    Update

--- includes/Specials/SpecialManageWiki.php
+++ includes/Specials/SpecialManageWiki.php
@@ -354,7 +354,8 @@ class SpecialManageWiki extends SpecialPage {
 				$mwNamespaces = new ManageWikiNamespaces( $dbname );
 				$create['out']['filter-callback'] = static fn ( string $value ): string => trim( $value );
 				$create['out']['validation-callback'] = fn ( string $value ): bool|Message =>
-					!$mwNamespaces->namespaceNameExists( $value ) ?: $this->msg( 'managewiki-namespace-exists' );
+					!$mwNamespaces->namespaceNameExists( $value ) ?:
+						$this->msg( 'managewiki-namespace-conflicts', $value );
 			}
 
 			$createForm = HTMLForm::factory( 'ooui', $hidden + $create, $this->getContext(), 'create' );
