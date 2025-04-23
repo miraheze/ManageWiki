c4454c5104e58ec09bbe080ef07a65d3c2547515
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-23 15:38:01 -0600

    phan: enable PhanPluginMixedKeyNoKey

--- .phan/config.php
+++ .phan/config.php
@@ -18,7 +18,6 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 
 $cfg['suppress_issue_types'] = [
 	'PhanAccessMethodInternal',
-	'PhanPluginMixedKeyNoKey',
 	'SecurityCheck-LikelyFalsePositive',
 ];
 
