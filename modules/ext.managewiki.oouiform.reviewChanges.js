8efdc52ea608abc2fa03d8984f2408adf6242860
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-23 19:11:05 -0600

    Localize reviewChanges

--- modules/ext.managewiki.oouiform.reviewChanges.js
+++ modules/ext.managewiki.oouiform.reviewChanges.js
@@ -7,7 +7,6 @@
 		OO.inheritClass( ProcessDialog, OO.ui.ProcessDialog );
 
 		ProcessDialog.static.name = 'managewiki-review';
-
 		ProcessDialog.static.title = mw.msg( 'managewiki-review-title' );
 
 		ProcessDialog.static.actions = [ {
@@ -23,63 +22,80 @@
 				expanded: false
 			} );
 
-			const dialog = this;
-			$( '#managewiki-review' ).on( 'click', () => {
-				dialog.content.$element.empty();
+			this.$body.append( this.content.$element );
+
+			$( '#managewiki-review' ).off( 'click.review' ).on( 'click.review', () => {
+				this.content.$element.empty();
 
 				const $inputs = $( '#managewiki-form :input[name]' )
 					.not( '#managewiki-submit-reason :input[name]' );
 
+				let hasChanges = false;
+
 				$inputs.each( function () {
+					const $input = $( this );
 					const name = this.name
 						.replace( 'wp', '' )
 						.replace( /-namespace|-namespacetalk|ext-|set-/, '' );
-					const label = $( this )
-						.parents( 'fieldset' )
+
+					const label = $input
+						.closest( 'fieldset' )
 						.contents()
 						.first()
-						.text();
+						.text()
+						.trim();
 
 					if (
 						this.type === 'checkbox' &&
 						this.defaultChecked !== undefined &&
 						this.defaultChecked !== this.checked
 					) {
-						dialog.content.$element.append(
-							`<li><b>${ name.replace( '[]', `[${ this.value }]` ) } (${ label })</b> was <i>${
-								this.checked ? 'enabled' : 'disabled'
-							}</i></li>`
-						);
+						const changeText = this.checked
+							? mw.msg( 'managewiki-review-enabled' )
+							: mw.msg( 'managewiki-review-disabled' );
+
+						const message = mw.message(
+							'managewiki-review-was-checkbox',
+							`${ name.replace( '[]', `[${ this.value }]` ) } (${ label })`,
+							changeText
+						).text();
+
+						this.content.$element.append( $( '<li>' ).text( message ) );
+						hasChanges = true;
 					} else if (
 						this.defaultValue !== undefined &&
 						this.defaultValue !== this.value
 					) {
-						const oldVal = this.defaultValue || '&lt;none&gt;';
-						const newVal = this.value || '&lt;none&gt;';
-						dialog.content.$element.append(
-							`<li><b>${ name } (${ label })</b> was changed from <i>${ oldVal }</i> to <i>${ newVal }</i></li>`
-						);
-					}
-				} );
+						const oldVal = this.defaultValue || mw.msg( 'managewiki-review-none' );
+						const newVal = this.value || mw.msg( 'managewiki-review-none' );
 
-				if ( !dialog.content.$element.html() ) {
-					dialog.content.$element.append( $( '<i>' ).text( 'No changes made.' ) );
+						const message = mw.message(
+							'managewiki-review-changed-from',
+							`${ name } (${ label })`,
+							oldVal,
+							newVal
+						).text();
+
+						this.content.$element.append( $( '<li>' ).text( message ) );
+						hasChanges = true;
+					}
+				}.bind( this ) );
+
+				if ( !hasChanges ) {
+					this.content.$element.append( $( '<i>' ).text( mw.msg( 'managewiki-review-nochanges' ) ) );
 				}
 
-				dialog.$body.append( dialog.content.$element );
+				const windowManager = OO.ui.getWindowManagerForElement( this.$element );
+				if ( windowManager ) {
+					windowManager.openWindow( this );
+				}
 			} );
 		};
 
 		ProcessDialog.prototype.getActionProcess = function ( action ) {
-			const dialog = this;
 			if ( action ) {
-				return new OO.ui.Process( () => {
-					dialog.close( {
-						action: action
-					} );
-				} );
+				return new OO.ui.Process( () => this.close( { action } ) );
 			}
-
 			return ProcessDialog.super.prototype.getActionProcess.call( this, action );
 		};
 
@@ -93,11 +109,6 @@
 		const processDialog = new ProcessDialog( {
 			size: 'large'
 		} );
-
 		windowManager.addWindows( [ processDialog ] );
-
-		$( '#managewiki-review' ).on( 'click', () => {
-			windowManager.openWindow( processDialog );
-		} );
 	} );
 }() );
