97389e08a40564412b57d99880fa102dd27685fa
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-24 11:10:23 -0600

    Add delete functionality

--- includes/Helpers/MessageUpdater.php
+++ includes/Helpers/MessageUpdater.php
@@ -4,23 +4,45 @@ namespace Miraheze\ManageWiki\Helpers;
 
 use MediaWiki\CommentStore\CommentStoreComment;
 use MediaWiki\Message\Message;
+use MediaWiki\Page\DeletePageFactory;
 use MediaWiki\Page\WikiPageFactory;
 use MediaWiki\Revision\SlotRecord;
 use MediaWiki\Title\TitleFactory;
 use MediaWiki\User\User;
+use Wikimedia\Message\ITextFormatter;
+use Wikimedia\Message\MessageValue;
 
 class MessageUpdater {
 
 	public function __construct(
+		private readonly DeletePageFactory $deletePageFactory,
+		private readonly ITextFormatter $textFormatter,
 		private readonly TitleFactory $titleFactory,
 		private readonly WikiPageFactory $wikiPageFactory
 	) {
 	}
 
+	public function doDelete(
+		string $name,
+		string $reason,
+		User $user
+	): void {
+		$title = $this->titleFactory->newFromText( $name, NS_MEDIAWIKI );
+		if ( $title === null ) {
+			return;
+		}
+
+		$reason = $this->textFormatter->format( MessageValue::new( $reason ) );
+
+		$page = $this->wikiPageFactory->newFromTitle( $title );
+		$deletePage = $this->deletePageFactory->newDeletePage( $page, $user );
+		$deletePage->deleteUnsafe( $reason );
+	}
+
 	public function doUpdate(
 		string $name,
 		string $content,
-		Message $summary,
+		string $summary,
 		User $user
 	): void {
 		$title = $this->titleFactory->newFromText( $name, NS_MEDIAWIKI );
@@ -35,6 +57,8 @@ class MessageUpdater {
 			SlotRecord::MAIN,
 			$page->getContentHandler()->makeContent( $content, $title )
 		);
+
+		$summary = $this->textFormatter->format( MessageValue::new( $summary ) );
 
 		$comment = CommentStoreComment::newUnsavedComment( $summary );
 		$updater->saveRevision( $comment );
