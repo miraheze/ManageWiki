6d4c1f71d208bf8b240e903d3ec86c5469fe1710
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-25 20:08:47 -0600

    Multibyte-safe

--- includes/Specials/SpecialManageWiki.php
+++ includes/Specials/SpecialManageWiki.php
@@ -347,26 +347,30 @@ class SpecialManageWiki extends SpecialPage {
 				'required' => true,
 			];
 
-			$language = $this->getLanguage();
 			if ( $module === 'permissions' ) {
 				// Groups should typically be lowercase so we do that here.
 				// Display names can be customized using interface messages.
-				$create['out']['filter-callback'] = static fn ( string $value ): string => $language->lc( trim( $value ) );
+				$create['out']['filter-callback'] = static fn ( string $value ): string => mb_strtolower( trim( $value ) );
 				$create['out']['validation-callback'] = [ $this, 'validateNewGroupName' ];
 			}
 
 			if ( $module === 'namespaces' ) {
 				// Handle namespace validation and normalization
 				$mwNamespaces = new ManageWikiNamespaces( $dbname );
+				// Multibyte-safe version of ucfirst
 				$create['out']['filter-callback'] = static fn ( string $value ): string =>
-					$language->ucfirst( trim( trim( $value ), '_:' ) );
+					preg_replace_callback(
+						'/^(\s*[_:]*)(.)(.*)$/us',
+						static fn ( array $m ): string => $m[1] . mb_strtoupper( $m[2], 'UTF-8' ) . $m[3],
+						trim( $value )
+					) ?? '';
 
 				$create['out']['validation-callback'] = function ( string $value ) use ( $mwNamespaces ): bool|Message {
-					$disallowed = array_map( [ $language, 'lc' ],
+					$disallowed = array_map( 'mb_strtolower',
 						$this->getConfig()->get( ConfigNames::NamespacesDisallowedNames )
 					);
 
-					if ( in_array( $language->lc( $value ), $disallowed, true ) ) {
+					if ( in_array( mb_strtolower( $value ), $disallowed, true ) ) {
 						return $this->msg( 'managewiki-error-disallowednamespace', $value );
 					}
 
