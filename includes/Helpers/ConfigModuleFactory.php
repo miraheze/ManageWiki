ce4d791e5adbb856d5b9cd6525a6efc94c638351
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-29 16:06:15 -0600

    Add newFromLocal

--- includes/Helpers/ConfigModuleFactory.php
+++ includes/Helpers/ConfigModuleFactory.php
@@ -3,6 +3,8 @@
 namespace Miraheze\ManageWiki\Helpers;
 
 use InvalidArgumentException;
+use MediaWiki\Config\ServiceOptions;
+use MediaWiki\MainConfigNames;
 use Miraheze\CreateWiki\IConfigModule;
 use Miraheze\CreateWiki\Services\RemoteWikiFactory;
 
@@ -10,16 +12,34 @@ class ConfigModuleFactory {
 
 	private const DEFAULT_DATABASE = 'default';
 
+	public const CONSTRUCTOR_OPTIONS = [
+		MainConfigNames::DBname,
+	];
+
 	public function __construct(
 		private readonly ManageWikiExtensions $extensions,
 		private readonly ManageWikiNamespaces $namespaces,
 		private readonly ManageWikiPermissions $permissions,
 		private readonly ManageWikiSettings $settings,
-		private readonly RemoteWikiFactory $core
+		private readonly RemoteWikiFactory $core,
+		private readonly ServiceOptions $options
 	) {
+		$options->assertRequiredOptions( self::CONSTRUCTOR_OPTIONS );
 	}
 
 	public function newFromDB( string $module, string $dbname ): IConfigModule {
+		return match ( $module ) {
+			'core' => $this->core->newInstance( $dbname ),
+			'extensions' => $this->extensions->newInstance( $dbname ),
+			'namespaces' => $this->namespaces->newInstance( $dbname ),
+			'permissions' => $this->permissions->newInstance( $dbname ),
+			'settings' => $this->settings->newInstance( $dbname ),
+			default => throw new InvalidArgumentException( "$module not recognized" ),
+		}
+	}
+
+	public function newFromLocal( string $module ): IConfigModule {
+		$dbname = $this->options->get( MainConfigNames::DBname );
 		return match ( $module ) {
 			'core' => $this->core->newInstance( $dbname ),
 			'extensions' => $this->extensions->newInstance( $dbname ),
