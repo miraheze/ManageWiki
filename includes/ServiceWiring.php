7fd32347eb00d5883391eb77d0132415b0fb6e1e
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-29 14:41:23 -0600

    Add

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -3,8 +3,13 @@
 namespace Miraheze\ManageWiki;
 
 use MediaWiki\Config\Config;
+use MediaWiki\Config\ServiceOptions;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
+use Miraheze\ManageWiki\Helpers\ManageWikiExtensions;
+use Miraheze\ManageWiki\Helpers\ManageWikiNamespaces;
+use Miraheze\ManageWiki\Helpers\ManageWikiPermissions;
+use Miraheze\ManageWiki\Helpers\ManageWikiSettings;
 use Miraheze\ManageWiki\Hooks\Handlers\CreateWiki;
 use Miraheze\ManageWiki\Hooks\ManageWikiHookRunner;
 use Psr\Log\LoggerInterface;
@@ -24,11 +29,55 @@ return [
 	'ManageWikiConfig' => static function ( MediaWikiServices $services ): Config {
 		return $services->getConfigFactory()->makeConfig( 'ManageWiki' );
 	},
+	'ManageWikiExtensions' => static function ( MediaWikiServices $services ): ManageWikiExtensions {
+		return new ManageWikiExtensions(
+			$services->get( 'CreateWikiDatabaseUtils' ),
+			$services->get( 'CreateWikiDataFactory' ),
+			$services->get( 'ManageWikiLogger' ),
+			new ServiceOptions(
+				ManageWikiExtensions::CONSTRUCTOR_OPTIONS,
+				$services->get( 'ManageWikiConfig' )
+			)
+		);
+	},
 	'ManageWikiHookRunner' => static function ( MediaWikiServices $services ): ManageWikiHookRunner {
 		return new ManageWikiHookRunner( $services->getHookContainer() );
 	},
 	'ManageWikiLogger' => static function (): LoggerInterface {
 		return LoggerFactory::getInstance( 'ManageWiki' );
+	},
+	'ManageWikiNamespaces' => static function ( MediaWikiServices $services ): ManageWikiNamespaces {
+		return new ManageWikiNamespaces(
+			$services->get( 'CreateWikiDatabaseUtils' ),
+			$services->get( 'CreateWikiDataFactory' ),
+			$services->getJobQueueGroupFactory(),
+			$services->getNamespaceInfo(),
+			new ServiceOptions(
+				ManageWikiNamespaces::CONSTRUCTOR_OPTIONS,
+				$services->get( 'ManageWikiConfig' )
+			)
+		);
+	},
+	'ManageWikiPermissions' => static function ( MediaWikiServices $services ): ManageWikiPermissions {
+		return new ManageWikiPermissions(
+			$services->get( 'CreateWikiDatabaseUtils' ),
+			$services->get( 'CreateWikiDataFactory' ),
+			$services->getActorStoreFactory(),
+			$services->getUserGroupManagerFactory(),
+			$services->getMessageFormatterFactory()->getTextFormatter(
+				$services->getContentLanguageCode()->toString()
+			),
+		);
+	},
+	'ManageWikiSettings' => static function ( MediaWikiServices $services ): ManageWikiSettings {
+		return new ManageWikiSettings(
+			$services->get( 'CreateWikiDatabaseUtils' ),
+			$services->get( 'CreateWikiDataFactory' ),
+			new ServiceOptions(
+				ManageWikiSettings::CONSTRUCTOR_OPTIONS,
+				$services->get( 'ManageWikiConfig' )
+			)
+		);
 	},
 ];
 
