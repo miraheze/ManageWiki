8d02ca355b772608777d02ef78b2349144839d00
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-29 12:08:20 -0600

    Move CLI check to permissions method in ManageWikiRequirements

--- includes/Helpers/ManageWikiRequirements.php
+++ includes/Helpers/ManageWikiRequirements.php
@@ -20,15 +20,11 @@ class ManageWikiRequirements {
 	 * @return bool Whether the extension can be enabled
 	 */
 	public static function process( array $actions, array $extList ): bool {
-		// Produces an array of steps and results (so we can fail what we can't do but apply what works)
 		$stepResponse = [];
-
 		foreach ( $actions as $action => $data ) {
 			switch ( $action ) {
 				case 'permissions':
-					// We don't check permissions if we are in CLI mode, so that we can
-					// toggle restricted extensions in CLI.
-					$stepResponse['permissions'] = PHP_SAPI === 'cli' || self::permissions( $data );
+					$stepResponse['permissions'] = self::permissions( $data );
 					break;
 				case 'extensions':
 					$stepResponse['extensions'] = self::extensions( $data, $extList );
@@ -64,6 +60,12 @@ class ManageWikiRequirements {
 	 * @return bool Whether permissions requirements are met
 	 */
 	private static function permissions( array $data ): bool {
+		// We don't check permissions if we are in CLI mode, so that we can
+		// toggle restricted extensions in CLI.
+		if ( MW_ENTRY_POINT === 'cli' ) {
+			return true;
+		}
+
 		$authority = RequestContext::getMain()->getAuthority();
 		return $authority->isAllowedAll( ...$data );
 	}
