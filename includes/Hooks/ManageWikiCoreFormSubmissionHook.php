188aa69386c5cef48ff03049dcb08c571697ac9b
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-04-29 19:27:54 -0600

    Remove

--- includes/Hooks/ManageWikiCoreFormSubmissionHook.php
+++ includes/Hooks/ManageWikiCoreFormSubmissionHook.php
@@ -4,13 +4,11 @@ namespace Miraheze\ManageWiki\Hooks;
 
 use MediaWiki\Context\IContextSource;
 use Miraheze\CreateWiki\Services\RemoteWikiFactory;
-use Wikimedia\Rdbms\IDatabase;
 
 interface ManageWikiCoreFormSubmissionHook {
 
 	/**
 	 * @param IContextSource $context
-	 * @param IDatabase $dbw
 	 * @param RemoteWikiFactory $remoteWiki
 	 * @param string $dbname
 	 * @param array $formData
@@ -18,7 +16,6 @@ interface ManageWikiCoreFormSubmissionHook {
 	 */
 	public function onManageWikiCoreFormSubmission(
 		IContextSource $context,
-		IDatabase $dbw,
 		RemoteWikiFactory $remoteWiki,
 		string $dbname,
 		array $formData
