04b61161f9e77281b1d3b9c33c6597a71db103ef
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-02 10:30:42 -0600

    Update

--- includes/Helpers/Factories/SettingsFactory.php
+++ includes/Helpers/Factories/SettingsFactory.php
@@ -3,23 +3,23 @@
 namespace Miraheze\ManageWiki\Helpers\Factories;
 
 use MediaWiki\Config\ServiceOptions;
-use Miraheze\CreateWiki\Services\CreateWikiDatabaseUtils;
 use Miraheze\CreateWiki\Services\CreateWikiDataFactory;
+use Miraheze\ManageWiki\DatabaseUtils;
 use Miraheze\ManageWiki\Helpers\ManageWikiSettings;
 
 class SettingsFactory {
 
 	public function __construct(
-		private readonly CreateWikiDatabaseUtils $databaseUtils,
 		private readonly CreateWikiDataFactory $dataFactory,
+		private readonly DatabaseUtils $databaseUtils,
 		private readonly ServiceOptions $options
 	) {
 	}
 
 	public function newInstance( string $dbname ): ManageWikiSettings {
 		return new ManageWikiSettings(
-			$this->databaseUtils,
 			$this->dataFactory,
+			$this->databaseUtils,
 			$this->options,
 			$dbname
 		);
