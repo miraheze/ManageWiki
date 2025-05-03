f58fc8be23ee9f0e109a51aa657182051f4add22
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-03 12:40:11 -0600

    Add the ability for extensions to register core module

---
+++ includes/ICoreModule.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Miraheze\ManageWiki;
+
+interface ICoreModule extends IModule {
+
+	public function getSitename(): string;
+	public function setSitename( string $sitename ): void;
+
+	public function getLanguage(): string;
+	public function setLanguage( string $lang ): void;
+
+	public function isInactive(): bool;
+	public function markInactive(): void;
+	public function markActive(): void;
+
+	public function getInactiveTimestamp(): ?string;
+	public function isInactiveExempt(): bool;
+	public function markExempt(): void;
+	public function unExempt(): void;
+	public function setInactiveExemptReason( string $reason ): void;
+	public function getInactiveExemptReason(): ?string;
+
+	public function isPrivate(): bool;
+	public function markPrivate(): void;
+	public function markPublic(): void;
+
+	public function isClosed(): bool;
+	public function markClosed(): void;
+
+	public function isDeleted(): bool;
+	public function delete(): void;
+	public function undelete(): void;
+
+	public function isLocked(): bool;
+	public function lock(): void;
+	public function unlock(): void;
+
+	public function getCategory(): string;
+	public function setCategory( string $category ): void;
+
+	public function getServerName(): string;
+	public function setServerName( string $server ): void;
+
+	public function getDBCluster(): string;
+	public function setDBCluster( string $dbcluster ): void;
+
+	public function isExperimental(): bool;
+	public function markExperimental(): void;
+	public function unMarkExperimental(): void;
+
+	public function trackChange( string $field, mixed $oldValue, mixed $newValue ): void;
+}
