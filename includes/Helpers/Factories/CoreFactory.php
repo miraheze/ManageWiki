5f6880c02760c4f9cc0c8fe0b19bd934973c735d
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-03 12:49:57 -0600

    Update

--- includes/Helpers/Factories/CoreFactory.php
+++ includes/Helpers/Factories/CoreFactory.php
@@ -2,34 +2,19 @@
 
 namespace Miraheze\ManageWiki\Helpers\Factories;
 
-use MediaWiki\Config\ServiceOptions;
-use MediaWiki\JobQueue\JobQueueGroupFactory;
-use Miraheze\CreateWiki\Helpers\RemoteWiki;
-use Miraheze\CreateWiki\Hooks\CreateWikiHookRunner;
-use Miraheze\CreateWiki\Services\CreateWikiDatabaseUtils;
-use Miraheze\CreateWiki\Services\CreateWikiDataFactory;
+use Miraheze\ManageWiki\Hooks\ManageWikiHookRunner;
+use Miraheze\ManageWiki\ICoreModule;
 
 class CoreFactory {
 
 	public function __construct(
-		private readonly CreateWikiDatabaseUtils $databaseUtils,
-		private readonly CreateWikiDataFactory $dataFactory,
-		private readonly CreateWikiHookRunner $hookRunner,
-		private readonly JobQueueGroupFactory $jobQueueGroupFactory,
-		private readonly ServiceOptions $options
+		private readonly ManageWikiHookRunner $hookRunner
 	) {
 	}
 
-	public function newInstance( string $dbname ): RemoteWiki {
-		// Temporary, we will eventually allow extensions to register
-		// the provider that we use for ManageWiki core.
-		return new RemoteWiki(
-			$this->databaseUtils,
-			$this->dataFactory,
-			$this->hookRunner,
-			$this->jobQueueGroupFactory,
-			$this->options,
-			$dbname
-		);
+	public function newInstance( string $dbname ): ?ICoreModule {
+		$provider = null;
+		$this->hookRunner->onManageWikiCoreProvider( $provider );
+		return $provider;
 	}
 }
