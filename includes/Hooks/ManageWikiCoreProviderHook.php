030419cb59c36930f71573ffc2b3fe95f01fb0ad
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-03 16:32:38 -0600

    nullable

--- includes/Hooks/ManageWikiCoreProviderHook.php
+++ includes/Hooks/ManageWikiCoreProviderHook.php
@@ -7,9 +7,9 @@ use Miraheze\ManageWiki\ICoreModule;
 interface ManageWikiCoreProviderHook {
 
 	/**
-	 * @param ICoreModule &$provider
+	 * @param ?ICoreModule &$provider
 	 * @param string $dbname
 	 * @return void
 	 */
-	public function onManageWikiCoreProvider( ICoreModule &$provider, string $dbname ): void;
+	public function onManageWikiCoreProvider( ?ICoreModule &$provider, string $dbname ): void;
 }
