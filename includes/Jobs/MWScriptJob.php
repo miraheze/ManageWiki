41c929f2ec6b31995f8418d5c14d0cea8e7ff775
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-03 20:07:46 -0600

    Update

--- includes/Jobs/MWScriptJob.php
+++ includes/Jobs/MWScriptJob.php
@@ -3,6 +3,8 @@
 namespace Miraheze\ManageWiki\Jobs;
 
 use Job;
+use JobSpecification;
+use MediaWiki\JobQueue\JobQueueGroupFactory;
 use MediaWiki\Shell\Shell;
 use Psr\Log\LoggerInterface;
 
@@ -17,6 +19,7 @@ class MWScriptJob extends Job {
 
 	public function __construct(
 		array $params,
+		private readonly JobQueueGroupFactory $jobQueueGroupFactory,
 		private readonly LoggerInterface $logger
 	) {
 		parent::__construct( self::JOB_NAME, $params );
@@ -30,10 +33,17 @@ class MWScriptJob extends Job {
 	 * @return bool
 	 */
 	public function run(): bool {
+		$repeatWith = [];
+		$options = $this->options;
+		if ( isset( $options['repeat-with'] ) ) {
+			$repeatWith = $options['repeat-with'];
+			unset( $options['repeat-with'] );
+		}
+
 		$limits = [ 'memory' => 0, 'filesize' => 0 ];
 		$arguments = [ '--wiki', $this->dbname ];
 
-		foreach ( $this->options as $name => $val ) {
+		foreach ( $options as $name => $val ) {
 			$arguments[] = "--$name";
 
 			if ( !is_bool( $val ) ) {
@@ -52,6 +62,20 @@ class MWScriptJob extends Job {
 				'result' => $result,
 				'script' => $this->script,
 			] );
+		}
+
+		if ( $repeatWith ) {
+			$jobQueueGroup = $this->jobQueueGroupFactory->makeJobQueueGroup();
+			$jobQueueGroup->push(
+				new JobSpecification(
+					self::JOB_NAME,
+					[
+						'dbname' => $this->dbname,
+						'script' => $this->script,
+						'options' => $repeatWith,
+					]
+				)
+			);
 		}
 
 		return true;
