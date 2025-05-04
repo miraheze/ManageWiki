e3e6f52cd9351b1b3a681776f3ca6a95018cb2f4
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-04 15:44:47 -0600

    Update

--- includes/Helpers/ManageWikiExtensionRegistry.php
+++ includes/Helpers/ManageWikiExtensionRegistry.php
@@ -6,6 +6,19 @@ use MediaWiki\Registration\ExtensionRegistry;
 
 class ManageWikiExtensionRegistry extends ExtensionRegistry {
 
+	/**
+	 * @var ExtensionRegistry
+	 */
+	private static $instance;
+
+	public static function getInstance() {
+		if ( self::$instance === null ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
 	public function queue( $path ) {
 		if ( str_ends_with( $path, 'WikibaseClient/extension.json' ) ) {
 			$path = str_replace( 'WikibaseClient/extension.json', 'Wikibase/extension-client.json', $path );
