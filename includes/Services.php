5f6ed61e9008b342b80effd69c40fa59157e09f5
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-14 11:30:15 -0600

    extend MediaWikiServices

--- includes/Services.php
+++ includes/Services.php
@@ -19,96 +19,82 @@ use Psr\Log\LoggerInterface;
  * A simple wrapper for MediaWikiServices, to support type safety when accessing
  * services defined by this extension.
  */
-class ManageWikiServices {
-
-	public function __construct(
-		private readonly MediaWikiServices $coreServices
-	) {
-	}
-
-	/**
-	 * Static version of the constructor, for nicer syntax.
-	 * @param MediaWikiServices $coreServices
-	 * @return static
-	 */
-	public static function wrap( MediaWikiServices $coreServices ): static {
-		return new static( $coreServices );
-	}
+class Services extends MediaWikiServices {
 
 	/**
 	 * @return Config ManageWiki configuration
 	 */
 	public function getConfig(): Config {
-		return $this->coreServices->get( 'ManageWikiConfig' );
+		return $this->getService( 'ManageWikiConfig' );
 	}
 
 	/**
 	 * @return CoreFactory Factory for core ManageWiki module
 	 */
 	public function getCoreFactory(): CoreFactory {
-		return $this->coreServices->get( 'ManageWikiCoreFactory' );
+		return $this->getService( 'ManageWikiCoreFactory' );
 	}
 
 	/**
 	 * @return DatabaseUtils Database utility methods
 	 */
 	public function getDatabaseUtils(): DatabaseUtils {
-		return $this->coreServices->get( 'ManageWikiDatabaseUtils' );
+		return $this->getService( 'ManageWikiDatabaseUtils' );
 	}
 
 	/**
 	 * @return DefaultPermissions Default permissions manager
 	 */
 	public function getDefaultPermissions(): DefaultPermissions {
-		return $this->coreServices->get( 'ManageWikiDefaultPermissions' );
+		return $this->getService( 'ManageWikiDefaultPermissions' );
 	}
 
 	/**
 	 * @return ExtensionsFactory Factory for the ManageWiki extensions module
 	 */
 	public function getExtensionsFactory(): ExtensionsFactory {
-		return $this->coreServices->get( 'ManageWikiExtensionsFactory' );
+		return $this->getService( 'ManageWikiExtensionsFactory' );
 	}
 
 	/**
 	 * @return ManageWikiHookRunner Hook runner for ManageWiki hooks
 	 */
 	public function getHookRunner(): ManageWikiHookRunner {
-		return $this->coreServices->get( 'ManageWikiHookRunner' );
+		return $this->getService( 'ManageWikiHookRunner' );
 	}
 
 	/**
 	 * @return LoggerInterface Logger for ManageWiki
 	 */
 	public function getLogger(): LoggerInterface {
-		return $this->coreServices->get( 'ManageWikiLogger' );
+		return $this->getService( 'ManageWikiLogger' );
 	}
 
 	/**
 	 * @return NamespacesFactory Factory for namespace configuration module
 	 */
 	public function getNamespacesFactory(): NamespacesFactory {
-		return $this->coreServices->get( 'ManageWikiNamespacesFactory' );
+		return $this->getService( 'ManageWikiNamespacesFactory' );
 	}
 
 	/**
 	 * @return PermissionsFactory Factory for permissions module
 	 */
 	public function getPermissionsFactory(): PermissionsFactory {
-		return $this->coreServices->get( 'ManageWikiPermissionsFactory' );
+		return $this->getService( 'ManageWikiPermissionsFactory' );
 	}
 
 	/**
 	 * @return SettingsFactory Factory for settings module
 	 */
 	public function getSettingsFactory(): SettingsFactory {
-		return $this->coreServices->get( 'ManageWikiSettingsFactory' );
+		return $this->getService( 'ManageWikiSettingsFactory' );
 	}
 
 	/**
 	 * @return ModuleFactory Factory for managing all modules (core, extensions, etc.)
 	 */
 	public function getModuleFactory(): ModuleFactory {
-		return $this->coreServices->get( 'ManageWikiModuleFactory' );
+		return $this->getService( 'ManageWikiModuleFactory' );
 	}
 }
