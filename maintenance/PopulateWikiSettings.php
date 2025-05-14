171cf9a2fa667fde6f46d07d7076a1b70f679248
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-14 10:46:36 -0600

    PopulateWikiSettings: rename --wgsetting to --setting

--- maintenance/PopulateWikiSettings.php
+++ maintenance/PopulateWikiSettings.php
@@ -9,9 +9,9 @@ class PopulateWikiSettings extends Maintenance {
 	public function __construct() {
 		parent::__construct();
 
-		$this->addOption( 'wgsetting', 'The $wg setting minus $.', true, true );
-		$this->addOption( 'sourcelist', 'File in format of "wiki|value" for the $wg setting above.', false, true );
-		$this->addOption( 'remove', 'Removes setting listed with --wgsetting.' );
+		$this->addOption( 'setting', 'The setting variable minus the $.', true, true );
+		$this->addOption( 'sourcelist', 'File in format of "wikidb|value" for the setting variable above.', false, true );
+		$this->addOption( 'remove', 'Removes setting listed with --setting.' );
 
 		$this->requireExtension( 'ManageWiki' );
 	}
@@ -20,7 +20,7 @@ class PopulateWikiSettings extends Maintenance {
 		$moduleFactory = $this->getServiceContainer()->get( 'ManageWikiModuleFactory' );
 		if ( $this->hasOption( 'remove' ) ) {
 			$mwSettings = $moduleFactory->settingsLocal();
-			$mwSettings->remove( [ $this->getOption( 'wgsetting' ) ], default: null );
+			$mwSettings->remove( [ $this->getOption( 'setting' ) ], default: null );
 			$mwSettings->commit();
 			return;
 		}
@@ -29,24 +29,26 @@ class PopulateWikiSettings extends Maintenance {
 			$this->fatalError( 'You must provide --sourcelist when not using --remove' );
 		}
 
-		$settingsource = file( $this->getOption( 'sourcelist' ) );
+		$settingSource = file( $this->getOption( 'sourcelist' ) );
 
-		foreach ( $settingsource as $input ) {
+		foreach ( $settingSource as $input ) {
 			$wikidb = explode( '|', $input, 2 );
-			[ $dbname, $settingvalue ] = array_pad( $wikidb, 2, '' );
+			[ $dbname, $settingValue ] = array_pad( $wikidb, 2, '' );
 
-			$this->output( "Setting $settingvalue for $dbname\n" );
+			$this->output( "Setting $settingValue for $dbname\n" );
 
-			$setting = str_replace( "\n", '', $settingvalue );
+			$setting = str_replace( "\n", '', $settingValue );
 
 			if ( $setting === 'true' ) {
 				$setting = true;
-			} elseif ( $setting === 'false' ) {
+			}
+
+			if ( $setting === 'false' ) {
 				$setting = false;
 			}
 
 			$mwSettings = $moduleFactory->settings( $dbname );
-			$mwSettings->modify( [ $this->getOption( 'wgsetting' ) => $setting ], default: null );
+			$mwSettings->modify( [ $this->getOption( 'setting' ) => $setting ], default: null );
 			$mwSettings->commit();
 		}
 	}
