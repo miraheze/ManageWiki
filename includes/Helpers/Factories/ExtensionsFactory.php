d2a8d3a18778c2922888bd575cc91da61a0ee74e
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-16 17:03:44 -0600

    Update

--- includes/Helpers/Factories/ExtensionsFactory.php
+++ includes/Helpers/Factories/ExtensionsFactory.php
@@ -5,6 +5,7 @@ namespace Miraheze\ManageWiki\Helpers\Factories;
 use MediaWiki\Config\ServiceOptions;
 use Miraheze\CreateWiki\Services\CreateWikiDataFactory;
 use Miraheze\ManageWiki\Helpers\ExtensionsModule;
+use Miraheze\ManageWiki\Helpers\Requirements;
 use Miraheze\ManageWiki\Helpers\Utils\DatabaseUtils;
 use Psr\Log\LoggerInterface;
 
@@ -15,6 +16,7 @@ class ExtensionsFactory {
 		private readonly DatabaseUtils $databaseUtils,
 		private readonly InstallerFactory $installerFactory,
 		private readonly LoggerInterface $logger,
+		private readonly Requirements $requirements,
 		private readonly ServiceOptions $options
 	) {
 	}
@@ -25,6 +27,7 @@ class ExtensionsFactory {
 			$this->databaseUtils,
 			$this->installerFactory,
 			$this->logger,
+			$this->requirements,
 			$this->options,
 			$dbname
 		);
