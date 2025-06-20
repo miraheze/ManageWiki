3578b3fbf99a3979ecf6933a7068777e71c753b4
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-06-20 14:05:44 -0600

    Use JSON datatype for JSON fields.
    Technically this is just an alias to LONGTEXT but it also adds a constraint ensuring valid JSON

--- sql/mw_permissions.sql
+++ sql/mw_permissions.sql
@@ -1,12 +1,12 @@
 CREATE TABLE /*_*/mw_permissions (
   `perm_dbname` VARCHAR(64) NOT NULL,
   `perm_group` VARCHAR(64) NOT NULL,
-  `perm_permissions` LONGTEXT NOT NULL,
-  `perm_addgroups` LONGTEXT NOT NULL,
-  `perm_removegroups` LONGTEXT NOT NULL,
-  `perm_addgroupstoself` LONGTEXT NOT NULL,
-  `perm_removegroupsfromself` LONGTEXT NOT NULL,
-  `perm_autopromote` LONGTEXT,
+  `perm_permissions` JSON NOT NULL,
+  `perm_addgroups` JSON NOT NULL,
+  `perm_removegroups` JSON NOT NULL,
+  `perm_addgroupstoself` JSON NOT NULL,
+  `perm_removegroupsfromself` JSON NOT NULL,
+  `perm_autopromote` JSON,
   UNIQUE KEY `uniqueperm`(perm_dbname,perm_group)
 ) /*$wgDBTableOptions*/;
 
