b3f291ead1cc6d766f9283a4479accf093c7c240
RhinosF1 <rhinosf1@gmail.com>
2025-07-29 16:02:38 +0100

    ToggleExtension: warn when disable not possible
    Avoid there being no output when nothing happens.
    
    Don't show when --all-wikis passed

--- maintenance/ToggleExtension.php
+++ maintenance/ToggleExtension.php
@@ -65,6 +65,9 @@ class ToggleExtension extends Maintenance {
 					$this->output( "Enabled $name on $dbname.\n" );
 				}
 			}
+			if ( !in_array( $name, $extList, true ) && $disable && !$allWikis) {
+				$this->output( "Failed to disable $name on $dbname.\n: Was it enabled?" );
+			}
 		}
 
 		if ( $noList && count( $dbnames ) > 1 ) {
