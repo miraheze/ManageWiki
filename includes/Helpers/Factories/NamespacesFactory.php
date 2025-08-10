0b194932917cda8a4acc15cf46f4db5348c588a1
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-10 17:07:58 -0600

    Update

--- includes/Helpers/Factories/NamespacesFactory.php
+++ includes/Helpers/Factories/NamespacesFactory.php
@@ -5,15 +5,14 @@ namespace Miraheze\ManageWiki\Helpers\Factories;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\JobQueue\JobQueueGroupFactory;
 use MediaWiki\Title\NamespaceInfo;
-use Miraheze\CreateWiki\Services\CreateWikiDataFactory;
 use Miraheze\ManageWiki\Helpers\NamespacesModule;
 use Miraheze\ManageWiki\Helpers\Utils\DatabaseUtils;
 
 class NamespacesFactory {
 
 	public function __construct(
-		private readonly CreateWikiDataFactory $dataFactory,
 		private readonly DatabaseUtils $databaseUtils,
+		private readonly DataFactory $dataFactory,
 		private readonly JobQueueGroupFactory $jobQueueGroupFactory,
 		private readonly NamespaceInfo $namespaceInfo,
 		private readonly ServiceOptions $options
@@ -22,8 +21,8 @@ class NamespacesFactory {
 
 	public function newInstance( string $dbname ): NamespacesModule {
 		return new NamespacesModule(
-			$this->dataFactory,
 			$this->databaseUtils,
+			$this->dataFactory,
 			$this->jobQueueGroupFactory,
 			$this->namespaceInfo,
 			$this->options,
