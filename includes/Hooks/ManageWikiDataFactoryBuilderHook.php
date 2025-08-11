2a6d72eeb8e986afbbdb9a91ba107bc3b5e1a9b3
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-11 10:13:01 -0600

    Add to hook

--- includes/Hooks/ManageWikiDataFactoryBuilderHook.php
+++ includes/Hooks/ManageWikiDataFactoryBuilderHook.php
@@ -2,11 +2,15 @@
 
 namespace Miraheze\ManageWiki\Hooks;
 
+use Miraheze\ManageWiki\Helpers\Factories\ModuleFactory;
+
 interface ManageWikiDataFactoryBuilderHook {
 
 	/**
 	 * @param string $dbname
 	 *   The target wiki's database name (e.g., "examplewiki").
+	 * @param ModuleFactory $moduleFactory
+	 *   ModuleFactory connection to be able to read module data.
 	 * @param array &$cacheArray
 	 *   The cache array that can be manipulated to add new entries to the
 	 *   ManageWiki cache for the individual wiki.
@@ -16,6 +20,7 @@ interface ManageWikiDataFactoryBuilderHook {
 	 */
 	public function onManageWikiDataFactoryBuilder(
 		string $dbname,
+		ModuleFactory $moduleFactory,
 		array &$cacheArray
 	): void;
 }
