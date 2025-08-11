8c8c090a71ed6d58fe159accf190c9ede7daedca
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-11 11:06:23 -0600

    Update

--- includes/Helpers/Factories/NamespacesFactory.php
+++ includes/Helpers/Factories/NamespacesFactory.php
@@ -2,18 +2,22 @@
 
 namespace Miraheze\ManageWiki\Helpers\Factories;
 
+use LocalisationCache;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\JobQueue\JobQueueGroupFactory;
 use MediaWiki\Title\NamespaceInfo;
 use Miraheze\ManageWiki\Helpers\NamespacesModule;
 use Miraheze\ManageWiki\Helpers\Utils\DatabaseUtils;
+use Psr\Log\LoggerInterface;
 
 class NamespacesFactory {
 
 	public function __construct(
 		private readonly DatabaseUtils $databaseUtils,
 		private readonly DataFactory $dataFactory,
+		private readonly LoggerInterface $logger,
 		private readonly JobQueueGroupFactory $jobQueueGroupFactory,
+		private readonly LocalisationCache $localisationCache,
 		private readonly NamespaceInfo $namespaceInfo,
 		private readonly ServiceOptions $options
 	) {
@@ -24,6 +28,8 @@ class NamespacesFactory {
 			$this->databaseUtils,
 			$this->dataFactory,
 			$this->jobQueueGroupFactory,
+			$this->localisationCache,
+			$this->logger,
 			$this->namespaceInfo,
 			$this->options,
 			$dbname
