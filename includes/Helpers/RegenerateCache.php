fb13a1411ad309f8ea69518f9ff271ef618ffe43
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-11 17:11:42 -0600

    Add a way to instantaneously refresh cache on all servers
    So that you dont have to wait until the second request for each particular server.

---
+++ includes/Helpers/RegenerateCache.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Miraheze\ManageWiki\Helpers;
+
+use MediaWiki\Config\ServiceOptions;
+use MediaWiki\Deferred\DeferredUpdates;
+use MediaWiki\Http\HttpRequestFactory;
+use MediaWiki\Utils\UrlUtils;
+use Miraheze\ManageWiki\ConfigNames;
+use Wikimedia\IPUtils;
+use function strlen;
+use const PROTO_INTERNAL;
+
+class CacheUpdate {
+
+	public const CONSTRUCTOR_OPTIONS = [
+		ConfigNames::Servers,
+	];
+
+	public function __construct(
+		private readonly HttpRequestFactory $httpRequestFactory,
+		private readonly UrlUtils $urlUtils,
+		private readonly ServiceOptions $options
+	) {
+		$options->assertRequiredOptions( self::CONSTRUCTOR_OPTIONS );
+	}
+
+	public function addUpdate(): void {
+		DeferredUpdates::addCallableUpdate( $this->doUpdate() );
+	}
+
+	public function doUpdate(): void {
+		$servers = $this->options->get( ConfigNames::Servers );
+		$reqs = [];
+		foreach ( $urls as $url ) {
+			$url = $this->urlUtils->expand( $url, PROTO_INTERNAL );
+			$urlInfo = $this->urlUtils->parse( $url ) ?? false;
+			$urlHost = strlen( $urlInfo['port'] ?? '' )
+				? IPUtils::combineHostAndPort( $urlInfo['host'], (int)$urlInfo['port'] )
+				: $urlInfo['host'];
+
+			$baseReq = [
+				'method' => 'PURGE',
+				'url' => $url,
+				'headers' => [
+					'Host' => $urlHost,
+					'Connection' => 'Keep-Alive',
+					'Proxy-Connection' => 'Keep-Alive',
+					'User-Agent' => 'ManageWiki extension',
+				]
+			];
+
+			foreach ( $servers as $server ) {
+				$reqs[] = ( $baseReq + [ 'proxy' => $server ] );
+			}
+		}
+
+		$http = $this->httpRequestFactory->createMultiClient( [
+			'maxConnsPerHost' => 8,
+			'usePipelining' => true,
+		] );
+
+		$http->runMulti( $reqs );
+	}
+}
