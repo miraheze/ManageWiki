d9d77bf2c2422f4ed7d22818deec1727c62da180
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-11 10:15:24 -0600

    Reorder

--- includes/Hooks/ManageWikiDataFactoryBuilderHook.php
+++ includes/Hooks/ManageWikiDataFactoryBuilderHook.php
@@ -7,10 +7,10 @@ use Miraheze\ManageWiki\Helpers\Factories\ModuleFactory;
 interface ManageWikiDataFactoryBuilderHook {
 
 	/**
-	 * @param string $dbname
-	 *   The target wiki's database name (e.g., "examplewiki").
 	 * @param ModuleFactory $moduleFactory
 	 *   ModuleFactory connection to be able to read module data.
+	 * @param string $dbname
+	 *   The target wiki's database name (e.g., "examplewiki").
 	 * @param array &$cacheArray
 	 *   The cache array that can be manipulated to add new entries to the
 	 *   ManageWiki cache for the individual wiki.
@@ -19,8 +19,8 @@ interface ManageWikiDataFactoryBuilderHook {
 	 * @codeCoverageIgnore Cannot be annotated as covered.
 	 */
 	public function onManageWikiDataFactoryBuilder(
-		string $dbname,
 		ModuleFactory $moduleFactory,
+		string $dbname,
 		array &$cacheArray
 	): void;
 }
