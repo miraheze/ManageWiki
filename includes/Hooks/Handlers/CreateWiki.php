db9c8b84497c63f0cbb99227a06724092383b3e9
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-11 11:15:22 -0600

    Remove

--- includes/Hooks/Handlers/CreateWiki.php
+++ includes/Hooks/Handlers/CreateWiki.php
@@ -2,33 +2,17 @@
 
 namespace Miraheze\ManageWiki\Hooks\Handlers;
 
-use Exception;
-use LocalisationCache;
 use MediaWiki\Config\Config;
 use Miraheze\CreateWiki\Hooks\CreateWikiCreationHook;
-use Miraheze\CreateWiki\Hooks\CreateWikiDataFactoryBuilderHook;
 use Miraheze\CreateWiki\Hooks\CreateWikiStatePrivateHook;
 use Miraheze\CreateWiki\Hooks\CreateWikiStatePublicHook;
 use Miraheze\CreateWiki\Hooks\CreateWikiTablesHook;
 use Miraheze\ManageWiki\ConfigNames;
 use Miraheze\ManageWiki\Helpers\DefaultPermissions;
 use Miraheze\ManageWiki\Helpers\Factories\ModuleFactory;
-use Psr\Log\LoggerInterface;
-use stdClass;
-use Wikimedia\Rdbms\IReadableDatabase;
-use Wikimedia\Rdbms\Platform\ISQLPlatform;
-use function array_merge;
-use function in_array;
-use function is_array;
-use function json_decode;
-use function str_replace;
-use const NS_PROJECT;
-use const NS_PROJECT_TALK;
-use const NS_SPECIAL;
 
 class CreateWiki implements
 	CreateWikiCreationHook,
-	CreateWikiDataFactoryBuilderHook,
 	CreateWikiStatePrivateHook,
 	CreateWikiStatePublicHook,
 	CreateWikiTablesHook
@@ -37,9 +21,7 @@ class CreateWiki implements
 	public function __construct(
 		private readonly Config $config,
 		private readonly DefaultPermissions $defaultPermissions,
-		private readonly LoggerInterface $logger,
-		private readonly ModuleFactory $moduleFactory,
-		private readonly LocalisationCache $localisationCache
+		private readonly ModuleFactory $moduleFactory
 	) {
 	}
 
@@ -72,141 +54,6 @@ class CreateWiki implements
 					maintainPrefix: false
 				);
 				$mwNamespaces->commit();
-			}
-		}
-	}
-
-	/** @inheritDoc */
-	public function onCreateWikiDataFactoryBuilder(
-		string $dbname,
-		IReadableDatabase $dbr,
-		array &$cacheArray
-	): void {
-		// Collate NS entries and decode their entries for the array
-		if ( $this->moduleFactory->isEnabled( 'namespaces' ) ) {
-			$nsObjects = $dbr->newSelectQueryBuilder()
-				->select( ISQLPlatform::ALL_ROWS )
-				->from( 'mw_namespaces' )
-				->where( [ 'ns_dbname' => $dbname ] )
-				->caller( __METHOD__ )
-				->fetchResultSet();
-
-			$metaNamespace = '';
-			$metaNamespaceTalk = '';
-
-			foreach ( $nsObjects as $ns ) {
-				if ( !$ns instanceof stdClass ) {
-					// Skip unexpected row
-					continue;
-				}
-
-				if ( $metaNamespace !== '' && $metaNamespaceTalk !== '' ) {
-					// Both found, no need to continue
-					break;
-				}
-
-				$id = (int)$ns->ns_namespace_id;
-
-				if ( $id === NS_PROJECT ) {
-					$metaNamespace = $ns->ns_namespace_name;
-					continue;
-				}
-
-				if ( $id === NS_PROJECT_TALK ) {
-					$metaNamespaceTalk = $ns->ns_namespace_name;
-				}
-			}
-
-			$lcName = [];
-			$lcEN = [];
-
-			try {
-				$languageCode = $cacheArray['core']['wgLanguageCode'] ?? 'en';
-				$lcName = $this->localisationCache->getItem( $languageCode, 'namespaceNames' );
-				$lcName[NS_PROJECT_TALK] = str_replace( '$1',
-					$lcName[NS_PROJECT] ?? $metaNamespace,
-					$lcName[NS_PROJECT_TALK] ?? $metaNamespaceTalk
-				);
-
-				if ( $languageCode !== 'en' ) {
-					$lcEN = $this->localisationCache->getItem( 'en', 'namespaceNames' );
-				}
-			} catch ( Exception $e ) {
-				$this->logger->error( 'Caught exception trying to load Localisation Cache: {exception}', [
-					'exception' => $e,
-				] );
-			}
-
-			$additional = $this->config->get( ConfigNames::NamespacesAdditional );
-			foreach ( $nsObjects as $ns ) {
-				if ( !$ns instanceof stdClass ) {
-					// Skip unexpected row
-					continue;
-				}
-
-				$nsName = $lcName[(int)$ns->ns_namespace_id] ?? $ns->ns_namespace_name;
-				$lcAlias = $lcEN[(int)$ns->ns_namespace_id] ?? null;
-
-				$cacheArray['namespaces'][$nsName] = [
-					'id' => (int)$ns->ns_namespace_id,
-					'core' => (bool)$ns->ns_core,
-					'searchable' => (bool)$ns->ns_searchable,
-					'subpages' => (bool)$ns->ns_subpages,
-					'content' => (bool)$ns->ns_content,
-					'contentmodel' => $ns->ns_content_model,
-					'protection' => $ns->ns_protection ?: false,
-					'aliases' => array_merge(
-						json_decode( str_replace( [ ' ', ':' ], '_', $ns->ns_aliases ?? '[]' ), true ),
-						(array)$lcAlias
-					),
-					'additional' => json_decode( $ns->ns_additional ?? '[]', true ),
-				];
-
-				$nsAdditional = json_decode( $ns->ns_additional ?? '[]', true );
-				foreach ( $additional as $var => $conf ) {
-					$nsID = (int)$ns->ns_namespace_id;
-
-					if ( !$this->isAdditionalSettingForNamespace( $conf, $nsID ) ) {
-						continue;
-					}
-
-					if ( isset( $nsAdditional[$var] ) ) {
-						$val = $nsAdditional[$var];
-					} elseif ( is_array( $conf['overridedefault'] ) ) {
-						$val = $conf['overridedefault'][$nsID]
-							?? $conf['overridedefault']['default']
-							?? null;
-
-						if ( $val === null ) {
-							// Skip if no fallback exists
-							continue;
-						}
-					} else {
-						$val = $conf['overridedefault'];
-					}
-
-					if ( $val ) {
-						$this->setNamespaceSettingCache( $cacheArray, $nsID, $var, $val, $conf );
-						continue;
-					}
-
-					if ( empty( $conf['constant'] ) && empty( $cacheArray['settings'][$var] ) ) {
-						$cacheArray['settings'][$var] = [];
-					}
-				}
-			}
-
-			// Search for and apply overridedefaults to NS_SPECIAL
-			// Notably, we do not apply 'default' overridedefault to NS_SPECIAL
-			// It must exist as it's own key in overridedefault
-			foreach ( $additional as $var => $conf ) {
-				if (
-					( $conf['overridedefault'][NS_SPECIAL] ?? false ) &&
-					$this->isAdditionalSettingForNamespace( $conf, NS_SPECIAL )
-				) {
-					$val = $conf['overridedefault'][NS_SPECIAL];
-					$this->setNamespaceSettingCache( $cacheArray, NS_SPECIAL, $var, $val, $conf );
-				}
 			}
 		}
 	}
@@ -250,55 +97,5 @@ class CreateWiki implements
 		if ( $this->moduleFactory->isEnabled( 'namespaces' ) ) {
 			$tables['mw_namespaces'] = 'ns_dbname';
 		}
-	}
-
-	/**
-	 * Adds the namespace setting for the supplied variable
-	 *
-	 * @param array &$cacheArray array for cache
-	 * @param int $nsID namespace ID number as an integer
-	 * @param string $var variable name
-	 * @param mixed $val variable value
-	 * @param array $varConf variable config from ConfigNames::NamespacesAdditional[$var]
-	 */
-	private function setNamespaceSettingCache(
-		array &$cacheArray,
-		int $nsID,
-		string $var,
-		mixed $val,
-		array $varConf
-	): void {
-		if ( $varConf['type'] === 'check' ) {
-			$cacheArray['settings'][$var][] = $nsID;
-			return;
-		}
-
-		if ( $varConf['type'] === 'vestyle' ) {
-			$cacheArray['settings'][$var][$nsID] = true;
-			return;
-		}
-
-		if ( $varConf['constant'] ?? false ) {
-			$cacheArray['settings'][$var] = str_replace( [ ' ', ':' ], '_', $val );
-			return;
-		}
-
-		$cacheArray['settings'][$var][$nsID] = $val;
-	}
-
-	/**
-	 * Checks if the namespace is for the additional setting given
-	 *
-	 * @param array $conf additional setting to check
-	 * @param int $nsID namespace ID to check if the setting is allowed for
-	 * @return bool Whether or not the setting is enabled for the namespace
-	 */
-	private function isAdditionalSettingForNamespace( array $conf, int $nsID ): bool {
-		// T12237: Do not apply additional settings if the setting is not for the
-		// namespace that we are on, otherwise it is very likely for the namespace to
-		// not have setting set, and cause settings set before to be ignored
-
-		$only = $conf['only'] ?? null;
-		return $only === null || in_array( $nsID, (array)$only, true );
 	}
 }
