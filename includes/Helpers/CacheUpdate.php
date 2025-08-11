df5a1e9a2a64ea6a444815211ee26b25919241ab
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-11 17:38:52 -0600

    Use MainPage URL

--- includes/Helpers/CacheUpdate.php
+++ includes/Helpers/CacheUpdate.php
@@ -5,6 +5,7 @@ namespace Miraheze\ManageWiki\Helpers;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\Deferred\DeferredUpdates;
 use MediaWiki\Http\HttpRequestFactory;
+use MediaWiki\Title\TitleFactory;
 use MediaWiki\Utils\UrlUtils;
 use Miraheze\ManageWiki\ConfigNames;
 use Wikimedia\IPUtils;
@@ -19,6 +20,7 @@ class CacheUpdate {
 
 	public function __construct(
 		private readonly HttpRequestFactory $httpRequestFactory,
+		private readonly TitleFactory $titleFactory,
 		private readonly UrlUtils $urlUtils,
 		private readonly ServiceOptions $options
 	) {
@@ -26,33 +28,34 @@ class CacheUpdate {
 	}
 
 	public function addUpdate(): void {
-		DeferredUpdates::addCallableUpdate( $this->doUpdate() );
+		DeferredUpdates::addCallableUpdate(
+			fn () => $this->doUpdate()
+		);
 	}
 
 	public function doUpdate(): void {
+		$mainPageUrl = $this->titleFactory->newMainPage()->getFullURL();
+		$url = $this->urlUtils->expand( $mainPageUrl, PROTO_INTERNAL );
+		$urlInfo = $this->urlUtils->parse( $url ) ?? false;
+		$urlHost = strlen( $urlInfo['port'] ?? '' )
+			? IPUtils::combineHostAndPort( $urlInfo['host'], (int)$urlInfo['port'] )
+			: $urlInfo['host'];
+
+		$baseReq = [
+			'method' => 'PURGE',
+			'url' => $url,
+			'headers' => [
+				'Host' => $urlHost,
+				'Connection' => 'Keep-Alive',
+				'Proxy-Connection' => 'Keep-Alive',
+				'User-Agent' => 'ManageWiki extension',
+			],
+		];
+
 		$servers = $this->options->get( ConfigNames::Servers );
 		$reqs = [];
-		foreach ( $urls as $url ) {
-			$url = $this->urlUtils->expand( $url, PROTO_INTERNAL );
-			$urlInfo = $this->urlUtils->parse( $url ) ?? false;
-			$urlHost = strlen( $urlInfo['port'] ?? '' )
-				? IPUtils::combineHostAndPort( $urlInfo['host'], (int)$urlInfo['port'] )
-				: $urlInfo['host'];
-
-			$baseReq = [
-				'method' => 'PURGE',
-				'url' => $url,
-				'headers' => [
-					'Host' => $urlHost,
-					'Connection' => 'Keep-Alive',
-					'Proxy-Connection' => 'Keep-Alive',
-					'User-Agent' => 'ManageWiki extension',
-				]
-			];
-
-			foreach ( $servers as $server ) {
-				$reqs[] = ( $baseReq + [ 'proxy' => $server ] );
-			}
+		foreach ( $servers as $server ) {
+			$reqs[] = ( $baseReq + [ 'proxy' => $server ] );
 		}
 
 		$http = $this->httpRequestFactory->createMultiClient( [
