d96f8e940d683dcb91ab14792ba2bdd71a0c7cae
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-09-18 17:44:13 -0600

    Add Rest API to reset cache

---
+++ includes/Rest/ResetWikiCacheHandler.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Miraheze\CreateWiki\RequestWiki\Rest;
+
+use MediaWiki\Rest\Response;
+use MediaWiki\Rest\SimpleHandler;
+use Miraheze\ManageWiki\ConfigNames;
+use Miraheze\ManageWiki\Helpers\Factories\DataStoreFactory;
+use SensitiveParameter
+use Wikimedia\Message\MessageValue;
+use Wikimedia\ParamValidator\ParamValidator;
+use function ctype_space;
+
+class ResetWikiCacheHandler extends SimpleHandler {
+
+	public function __construct(
+		private readonly DataStoreFactory $dataStoreFactory
+	) {
+	}
+
+	public function run(
+		string $dbname,
+		#[SensitiveParameter]
+		string $key
+	): Response {
+		if ( $key !== $this->getConfig()->get( ConfigNames::CacheUpdateKey ) ) {
+			return $this->getResponseFactory()->createLocalizedHttpError(
+				403, new MessageValue( 'managewiki-rest-invalidkey' )
+			);
+		}
+
+		$dataStore = $this->dataStoreFactory->newInstance( $dbname );
+		$dataStore->resetWikiData( isNewChanges: true );
+		return $this->getResponseFactory()->createNoContent();
+	}
+
+	public function needsWriteAccess(): bool {
+		return true;
+	}
+
+	public function getParamSettings(): array {
+		return [
+			'dbname' => [
+				self::PARAM_SOURCE => 'path',
+				ParamValidator::PARAM_TYPE => 'string',
+				ParamValidator::PARAM_REQUIRED => true,
+			],
+			'key' => [
+				self::PARAM_SOURCE => 'path',
+				ParamValidator::PARAM_TYPE => 'string',
+				ParamValidator::PARAM_REQUIRED => true,
+			],
+		];
+	}
+}
