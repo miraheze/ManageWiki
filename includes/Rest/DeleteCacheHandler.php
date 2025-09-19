0ed4f81a3961272bf7d014e57b4bbe67e7aac3ea
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-09-18 18:49:22 -0600

    Add DeleteCacheHandler

---
+++ includes/Rest/DeleteCacheHandler.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Miraheze\CreateWiki\RequestWiki\Rest;
+
+use MediaWiki\Config\Config;
+use MediaWiki\Rest\Response;
+use MediaWiki\Rest\SimpleHandler;
+use Miraheze\ManageWiki\ConfigNames;
+use SensitiveParameter;
+use Wikimedia\Message\MessageValue;
+use Wikimedia\ParamValidator\ParamValidator;
+
+class DeleteCacheHandler extends SimpleHandler {
+
+	public function __construct( private readonly Config $config ) {
+	}
+
+	public function run(
+		string $dbname,
+		#[SensitiveParameter]
+		string $key
+	): Response {
+		if ( $key !== $this->config->get( ConfigNames::CacheUpdateKey ) ) {
+			return $this->getResponseFactory()->createLocalizedHttpError(
+				403, new MessageValue( 'managewiki-rest-invalidkey' )
+			);
+		}
+
+		$this->config->get( ConfigNames::CacheDirectory )
+		if ( file_exists( "$cacheDir/$dbname.php" ) ) {
+			unlink( "$cacheDir/$dbname.php" );
+		}
+
+		return $this->getResponseFactory()->createNoContent();
+	}
+
+	public function needsWriteAccess(): bool {
+		return true;
+	}
+
+	public function getParamSettings(): array {
+		return [
+			'dbname' => [
+				self::PARAM_SOURCE => 'path',
+				ParamValidator::PARAM_TYPE => 'string',
+				ParamValidator::PARAM_REQUIRED => true,
+			],
+			'key' => [
+				self::PARAM_SOURCE => 'path',
+				ParamValidator::PARAM_TYPE => 'string',
+				ParamValidator::PARAM_REQUIRED => true,
+			],
+		];
+	}
+}
