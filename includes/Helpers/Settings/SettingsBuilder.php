482148b702a627ba1a13c84c190a667fb7504f1c
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-02-02 12:18:12 -0700

    Rename

--- includes/Helpers/Settings/SettingsBuilder.php
+++ includes/Helpers/Settings/SettingsBuilder.php
@@ -4,10 +4,10 @@ namespace Miraheze\ManageWiki\Helpers\Settings;
 
 use MediaWiki\Settings\Config\GlobalConfigBuilder;
 use MediaWiki\Settings\Config\PhpIniSink;
-use MediaWiki\Settings\SettingsBuilder;
+use MediaWiki\Settings\SettingsBuilder as CoreSettingsBuilder;
 use const MW_INSTALL_PATH;
 
-class ManageWikiSettingsBuilder extends SettingsBuilder {
+class SettingsBuilder extends CoreSettingsBuilder {
 
 	public static function getInstance(): self {
 		static $instance = null;
