a4f852cecfbc83ed13b782142989e96bcd8bf3f8
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-06 10:52:16 -0600

    Add blocking

--- includes/Helpers/CacheUpdate.php
+++ includes/Helpers/CacheUpdate.php
@@ -3,19 +3,34 @@
 namespace Miraheze\ManageWiki\Helpers;
 
 use MediaWiki\Config\ServiceOptions;
+use MediaWiki\Http\HttpRequestFactory;
 use MediaWiki\JobQueue\JobQueueGroupFactory;
 use MediaWiki\JobQueue\JobSpecification;
+use MediaWiki\MainConfigNames;
 use Miraheze\ManageWiki\ConfigNames;
 use Miraheze\ManageWiki\Jobs\CacheUpdateJob;
+use Psr\Log\LoggerInterface;
+use function count;
+use function http_build_query;
+use function implode;
+use function in_array;
+use function rawurlencode;
 
 class CacheUpdate {
 
 	public const array CONSTRUCTOR_OPTIONS = [
+		ConfigNames::CacheUpdateDebugHeader,
+		ConfigNames::CacheUpdateDomain,
+		ConfigNames::CacheUpdateKey,
+		ConfigNames::CacheUpdateRestEnabled,
 		ConfigNames::Servers,
+		MainConfigNames::RestPath,
 	];
 
 	public function __construct(
+		private readonly HttpRequestFactory $httpRequestFactory,
 		private readonly JobQueueGroupFactory $jobQueueGroupFactory,
+		private readonly LoggerInterface $logger,
 		private readonly ServiceOptions $options,
 	) {
 		$options->assertRequiredOptions( self::CONSTRUCTOR_OPTIONS );
@@ -33,5 +48,90 @@ class CacheUpdate {
 				'dbname' => $dbname,
 			] )
 		);
+	}
+
+	public function executeNow( string $action, string $dbname ): bool {
+		$servers = $this->options->get( ConfigNames::Servers );
+		if ( $servers === [] ) {
+			return true;
+		}
+
+		if ( !$this->options->get( ConfigNames::CacheUpdateRestEnabled ) ) {
+			$this->logger->error(
+				'CacheUpdate::executeNow can not run, ManageWikiCacheUpdateRestEnabled is disabled.'
+			);
+
+			return true;
+		}
+
+		$key = (string)$this->options->get( ConfigNames::CacheUpdateKey );
+		$domain = (string)$this->options->get( ConfigNames::CacheUpdateDomain );
+		$debugHeader = (string)$this->options->get( ConfigNames::CacheUpdateDebugHeader );
+
+		if ( $key === '' || $domain === '' || $debugHeader === '' ) {
+			$this->logger->error(
+				'CacheUpdate::executeNow can not run, one of ManageWikiCacheUpdateKey, ' .
+				'ManageWikiCacheUpdateDomain, or ManageWikiCacheUpdateDebugHeader ' .
+				'is not configured.'
+			);
+
+			return true;
+		}
+
+		if ( !in_array( $action, [ 'delete', 'reset' ], true ) ) {
+			$this->logger->error(
+				'CacheUpdate::executeNow can not run, action can only be delete or reset but it was set to {action}.',
+				[ 'action' => $action ]
+			);
+
+			return true;
+		}
+
+		$restPath = $this->options->get( MainConfigNames::RestPath );
+		$url = "https://$domain$restPath/managewiki/v0/cache/$action/$dbname";
+		$body = http_build_query( [ 'key' => $key ] );
+
+		$requests = [];
+		foreach ( $servers as $server ) {
+			$requests[$server] = [
+				'method' => 'POST',
+				'url' => $url,
+				'body' => $body,
+				'headers' => [
+					'Content-Type' => 'application/x-www-form-urlencoded',
+					$debugHeader => $server,
+				],
+			];
+		}
+
+		$http = $this->httpRequestFactory->createMultiClient( [
+			'maxConnsPerHost' => 8,
+			'usePipelining' => true,
+		] );
+
+		$responses = $http->runMulti( $requests );
+
+		$failed = [];
+		foreach ( $responses as $server => $requestResult ) {
+			$code = $requestResult['response']['code'] ?? 0;
+			if ( $code !== 204 ) {
+				$failed[] = $server;
+			}
+		}
+
+		if ( $failed !== [] ) {
+			$this->logger->error(
+				'CacheUpdate::executeNow failed on {count} server(s) for {dbname}: {servers}',
+				[
+					'count' => count( $failed ),
+					'dbname' => $dbname,
+					'servers' => implode( ', ', $failed ),
+				]
+			);
+
+			return false;
+		}
+
+		return true;
 	}
 }
