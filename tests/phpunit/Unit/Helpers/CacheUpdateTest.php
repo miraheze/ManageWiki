f3450fae137d8ea4f60a3074c473287891b6a1c2
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-07 10:45:02 -0600

    Update

--- tests/phpunit/Unit/Helpers/CacheUpdateTest.php
+++ tests/phpunit/Unit/Helpers/CacheUpdateTest.php
@@ -293,7 +293,7 @@ class CacheUpdateTest extends MediaWikiUnitTestCase {
 		$capturedBody = '';
 		$multiClient = $this->createMock( MultiHttpClient::class );
 		$multiClient->method( 'runMulti' )->willReturnCallback(
-			/** @return array<mixed, array<string, array<string, int>>> */
+			/** @return associative-array<mixed, array<string, array<string, int>>> */
 			static function ( array $requests ) use ( &$capturedBody ): array {
 				$capturedBody = (string)reset( $requests )['body'];
 				$responses = [];
