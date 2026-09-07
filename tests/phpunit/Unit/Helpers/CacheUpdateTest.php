dee18279d5b59150508fd6853a8e3abda57d0fd0
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-07 10:50:21 -0600

    Update

--- tests/phpunit/Unit/Helpers/CacheUpdateTest.php
+++ tests/phpunit/Unit/Helpers/CacheUpdateTest.php
@@ -259,7 +259,7 @@ class CacheUpdateTest extends MediaWikiUnitTestCase {
 		$capturedUrl = '';
 		$multiClient = $this->createMock( MultiHttpClient::class );
 		$multiClient->method( 'runMulti' )->willReturnCallback(
-			/** @return array<array-key, array{response: array{code: int}}> */
+			/** @phan-return array<array-key, array{response: array{code: int}}> */
 			static function ( array $requests ) use ( &$capturedUrl ): array {
 				$capturedUrl = (string)reset( $requests )['url'];
 				$responses = [];
@@ -293,7 +293,7 @@ class CacheUpdateTest extends MediaWikiUnitTestCase {
 		$capturedBody = '';
 		$multiClient = $this->createMock( MultiHttpClient::class );
 		$multiClient->method( 'runMulti' )->willReturnCallback(
-			/** @return associative-array<mixed, array<string, array<string, int>>> */
+			/** @phan-return associative-array<mixed, array<string, array<string, int>>> */
 			static function ( array $requests ) use ( &$capturedBody ): array {
 				$capturedBody = (string)reset( $requests )['body'];
 				$responses = [];
@@ -314,8 +314,7 @@ class CacheUpdateTest extends MediaWikiUnitTestCase {
 
 		$cacheUpdate->executeNow( 'reset', 'examplewiki' );
 
-		$decoded = json_decode( $capturedBody, true );
-		$this->assertIsArray( $decoded );
+		$decoded = json_decode( $capturedBody, true ) ?? [];
 		$this->assertSame( 'secret-key', $decoded['key'] ?? '' );
 	}
 
