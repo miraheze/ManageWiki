d6e23c2781fc3eb797b2fe206dcf9a9af96ad348
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-22 17:20:06 -0600

    Update

--- includes/Helpers/NamespacesModule.php
+++ includes/Helpers/NamespacesModule.php
@@ -3,10 +3,10 @@
 namespace Miraheze\ManageWiki\Helpers;
 
 use Exception;
-use LocalisationCache;
 use MediaWiki\Config\ServiceOptions;
 use MediaWiki\JobQueue\JobQueueGroupFactory;
 use MediaWiki\JobQueue\JobSpecification;
+use MediaWiki\Language\LocalisationCache;
 use MediaWiki\MainConfigNames;
 use MediaWiki\Title\NamespaceInfo;
 use Miraheze\ManageWiki\ConfigNames;
